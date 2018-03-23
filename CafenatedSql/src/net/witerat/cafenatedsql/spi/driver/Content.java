9e5391881e4c040182ec21e434dc6e19f1cc9a12
John Hutcheson <witerat.test@gmail.com>
2018-03-23 21:18:00 +1300

    JSLint revision

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/Content.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/Content.java
@@ -2,20 +2,21 @@ package net.witerat.cafenatedsql.spi.driver;
 
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 
-// TODO: Auto-generated Javadoc
 /**
  * Implementor return text dynamically using the provided model.
- * 
+ *
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
  *
  */
 public interface Content {
 
-	/**
-	 * Gets dynamically generated text according to evaluation of model content.
-	 *
-	 * @param model the model
-	 * @return fully expanded text including referenced macro and model injections.
-	 */
-	String getText(TemplateEngineModel model);
+  /**
+   * Gets dynamically generated text according to evaluation of model content.
+   *
+   * @param model
+   *          the model
+   * @return fully expanded text including referenced macro and model
+   *         injections.
+   */
+  String getText(TemplateEngineModel model);
 }
