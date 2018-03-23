34daa8b656c261f96b31134f3f5ae442e1e79e30
John Hutcheson <witerat.test@gmail.com>
2018-03-23 20:01:16 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/api/ConnectionFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/ConnectionFactory.java
@@ -2,46 +2,60 @@ package net.witerat.cafenatedsql.api;
 
 import java.sql.Connection;
 
-// TODO: Auto-generated Javadoc
 /**
  * A factory for creating Connection objects.
  */
 public interface ConnectionFactory extends CafenatedFactory {
-	
-	/** The default property name for identifying a database.  */
-	final static String DATABASE_NAME="DATABASE_NAME";  
-	
-	/**  The property for holding the user name. */
-	final static String USER_NAME="USER";  
-	
-	/** The property name for the password. */
-	final static String PASSWORD="PASSWORD";  
-	
-	/**
-	 * Sets a specified  property.
-	 *
-	 * @param property the property identity
-	 * @param Value the value the new value of the property.
-	 */
-	void setProperty(Object property, Object Value);
-	
-	/**
-	 * Gets the identified property.
-	 *
-	 * @param property the property
-	 * @return the property
-	 */
-	Object getProperty(Object property);
-	
-	/**
-	 * Establish a connection.
-	 *
-	 * @return the connection
-	 */
-	Connection connect();
 
-	Class<?> getPropertyType(Object property);
+  /** The default property name for identifying a database. */
+  String DATABASE_NAME = "DATABASE_NAME";
 
-	boolean isPropertyRequired(Object property);
+  /** The property for holding the user name. */
+  String USER_NAME = "USER";
+
+  /** The property name for the password. */
+  String PASSWORD = "PASSWORD";
+
+  /**
+   * Sets a specified property.
+   *
+   * @param property
+   *          the property identity
+   * @param value
+   *          the value the new value of the property.
+   */
+  void setProperty(Object property, Object value);
+
+  /**
+   * Gets the identified property.
+   *
+   * @param property
+   *          the property
+   * @return the property
+   */
+  Object getProperty(Object property);
+
+  /**
+   * Establish a connection.
+   *
+   * @return the connection
+   */
+  Connection connect();
+
+  /**
+   * Get class for values of a specified property.
+   * @param property an identifier for a property.
+   * @return A class which may contain all possible values of the specified
+   *    property.
+   */
+  Class<?> getPropertyType(Object property);
+
+  /**
+   * Reports whether a property is required.
+   * @param property an identifier for a property.
+   * @return <code>true</code> if the value of the property must be set for
+   *    the model to pass validation.
+   */
+  boolean isPropertyRequired(Object property);
 
 }
