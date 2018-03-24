dee91d20b2248a9080a89060b83b03de53f8299b
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:49:50 +1300

    JSLint review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/DialectDef.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/DialectDef.java
@@ -1,37 +1,40 @@
 package net.witerat.cafenatedsql.api.driver;
 
-
-// TODO: Auto-generated Javadoc
 /**
  * The Interface DialectDef.
  */
 public interface DialectDef extends ParameterizedRequest {
 
-	/**
-	 * Sets the name of this dialect.
-	 *
-	 * @param id the new name
-	 */
-	void setName(String id);
+  /**
+   * Sets the name of this dialect.
+   *
+   * @param id
+   *          the new name
+   */
+  void setName(String id);
 
-	/**
-	 * Gets the id of this dialect.
-	 *
-	 * @return the id
-	 */
-	String getId();
+  /**
+   * Gets the id of this dialect.
+   *
+   * @return the id
+   */
+  String getId();
 
-	/**
-	 * Adds a grammar to this dialect.
-	 *
-	 * @param g the g
-	 */
-	void addGrammar(GrammarDef g);
+  /**
+   * Adds a grammar to this dialect.
+   *
+   * @param g
+   *          the g
+   */
+  void addGrammar(GrammarDef g);
 
-	/**
-	 * Adds a parameter to this dialect definition
-	 * @see net.witerat.cafenatedsql.api.driver.ParameterizedRequest#add(net.witerat.cafenatedsql.api.driver.ParameterType)
-	 */
-	void add(ParameterType param);
+  /**
+   * Adds a parameter to this dialect definition.
+   *
+   * @param param the parameters.
+   * @see net.witerat.cafenatedsql.api.driver.ParameterizedRequest#add(
+   *    net.witerat.cafenatedsql.api.driver.ParameterType)
+   */
+  void add(ParameterType param);
 
 }
