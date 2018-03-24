fffeea102fbf9835724e08d959d63b247d8e1c2d
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:45:49 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/DefTag.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/DefTag.java
@@ -8,42 +8,55 @@ import javax.xml.bind.annotation.XmlElementRefs;
 import javax.xml.bind.annotation.XmlMixed;
 import javax.xml.bind.annotation.XmlType;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class DefTag.
+ * The Class DefTag describes a macro definition..
  */
-@XmlType(name="DefType", namespace="-//org.witerat/cafenated/sql")
-public class DefTag extends UrlTag { 
-    
-    /** The name. */
-    String name;
-    
-	/**
-	 * Gets the name.
-	 *
-	 * @return the name
-	 */
-    @XmlAttribute
-	public String getName() {
-		return name;
-	}
+@XmlType(name = "DefType", namespace = "-//org.witerat/cafenated/sql")
+public class DefTag extends UrlTag {
 
-	/**
-	 * Sets the name.
-	 *
-	 * @param name the new name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	@XmlElementRefs({
-		@XmlElementRef(name="use", namespace="-//org.witerat/cafenated/sql", type=UseTag.class),
-		@XmlElementRef(name="param", namespace="-//org.witerat/cafenated/sql", type=ParameterTag.class)})
-	@XmlMixed
-	public List<Object> getRawContent() {
-		return super.getRawContent();
-	}
-	public void setRawContent(List<Object> raw) {
-		super.setRawContent(raw);
-	}
+  /** The name. */
+  private String name;
+
+  /**
+   * Gets the name.
+   *
+   * @return the name
+   */
+  @XmlAttribute
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the name.
+   *
+   * @param name0
+   *          the new name
+   */
+  public void setName(final String name0) {
+    this.name = name0;
+  }
+
+  /**
+   * Gets the tags and text.
+   * @return a list of tags and text.
+   */
+  @XmlElementRefs({
+      @XmlElementRef(name = "use", type = UseTag.class,
+          namespace = "-//org.witerat/cafenated/sql"),
+      @XmlElementRef(name = "param", type = ParameterTag.class,
+          namespace = "-//org.witerat/cafenated/sql") })
+  @XmlMixed
+  public List<Object> getRawContent() {
+    return super.getRawContent();
+  }
+
+
+  /**
+   * Sets tags and text.
+   * @param raw a list of tags and text.
+   */
+  public void setRawContent(final List<Object> raw) {
+    super.setRawContent(raw);
+  }
 }
