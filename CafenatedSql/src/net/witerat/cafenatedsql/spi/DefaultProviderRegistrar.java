f072a595ac91a0e2fd698d23766e5b0fac6a0cba
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:42:02 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/DefaultProviderRegistrar.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/DefaultProviderRegistrar.java
@@ -5,91 +5,121 @@ import java.util.Collection;
 import net.witerat.cafenatedsql.api.Provider;
 import net.witerat.cafenatedsql.api.ProviderRegistrar;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class DefaultProviderRegistrar.
  *
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
  */
 public class DefaultProviderRegistrar implements ProviderRegistrar {
-	
-	/** The provider registrar. */
-	static ProviderRegistrar providerRegistrar;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getFactory(java.lang.String)
-	 */
-	public Provider getProvider(String name) {
-		return providerRegistrar.getProvider(name);
-	}
+  /** The provider registrar. */
+  private static ProviderRegistrar providerRegistrar;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getFactoryNames()
-	 */
-	public Collection<String> getProviderNames() {
-		return providerRegistrar.getProviderNames();
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#getFactory(java.lang.String)
+   */
+  public Provider getProvider(final String name) {
+    return providerRegistrar.getProvider(name);
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#registerVendor(java.lang.String, java.lang.String, java.lang.String)
-	 */
-	public void registerVendor(String vendor, String vendorName, String vendorLongName) {
-		providerRegistrar.registerVendor(vendor, vendorName, vendorLongName);
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getFactoryNames()
+   */
+  public Collection<String> getProviderNames() {
+    return providerRegistrar.getProviderNames();
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#registerFactory(net.witerat.cafenatedsql.api.driver.DriverFactory, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
-	 */
-	public void registerProvider(Provider provider, String name, String copyright, String longName,
-			String description, String vendor) {
-		providerRegistrar.registerProvider(provider, name, copyright, longName, description, vendor);
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#registerVendor(java.lang.
+   * String, java.lang.String, java.lang.String)
+   */
+  public void registerVendor(final String vendor, final String vendorName,
+      final String vendorLongName) {
+    providerRegistrar.registerVendor(vendor, vendorName, vendorLongName);
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getVendorLongName(java.lang.String)
-	 */
-	public String getVendorLongName(String vendor) {
-		return providerRegistrar.getVendorLongName(vendor);
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#registerFactory(
+   * net.witerat.cafenatedsql.api.driver.DriverFactory,
+   * java.lang.String, java.lang.String,java.lang.String,
+   * java.lang.String, java.lang.String)
+   */
+  public void registerProvider(final Provider provider, final String name,
+      final String copyright,
+      final String longName, final String description, final String vendor) {
+    providerRegistrar.registerProvider(provider, name, copyright,
+        longName, description, vendor);
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getVendorName(java.lang.String)
-	 */
-	public String getVendorName(String vendor) {
-		return providerRegistrar.getVendorName(vendor);
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#getVendorLongName(java.lang.
+   * String)
+   */
+  public String getVendorLongName(final String vendor) {
+    return providerRegistrar.getVendorLongName(vendor);
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getVendor(java.lang.String)
-	 */
-	public String getVendor(String name) {
-		return providerRegistrar.getVendor(name);
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#getVendorName(java.lang.
+   * String)
+   */
+  public String getVendorName(final String vendor) {
+    return providerRegistrar.getVendorName(vendor);
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.ProviderRegistrar#getVendors()
-	 */
-	public Collection<String> getVendors() {
-		return providerRegistrar.getVendors();
-	}
+  /**
+   * {@inheritDoc}
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.ProviderRegistrar#getVendor(java.lang.String)
+   */
+  public String getVendor(final String name) {
+    return providerRegistrar.getVendor(name);
+  }
 
-	/**
-	 * Gets the single instance of DefaultProviderRegistrar.
-	 *
-	 * @return single instance of DefaultProviderRegistrar
-	 */
-	static ProviderRegistrar getInstance() {
-		if (providerRegistrar == null) {
-			providerRegistrar = new SimpleProviderRegistrar();
-		}
-		return providerRegistrar;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public Collection<String> getVendors() {
+    return providerRegistrar.getVendors();
+  }
 
-	@Override
-	public String getDescription(String string) {
-		// TODO Auto-generated method stub
-		return providerRegistrar.getDescription(string);
-	}
+  /**
+   * Gets the single instance of DefaultProviderRegistrar.
+   *
+   * @return single instance of DefaultProviderRegistrar
+   */
+  static ProviderRegistrar getInstance() {
+    if (providerRegistrar == null) {
+      providerRegistrar = new SimpleProviderRegistrar();
+    }
+    return providerRegistrar;
+  }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getDescription(final String string) {
+    // TODO Auto-generated method stub
+    return providerRegistrar.getDescription(string);
+  }
 
 }
