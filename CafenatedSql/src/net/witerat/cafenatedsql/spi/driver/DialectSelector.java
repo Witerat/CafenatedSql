408f61a7a745c9bbe0015a439860b61d972d6c56
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:54:47 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/DialectSelector.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/DialectSelector.java
@@ -4,27 +4,30 @@ import net.witerat.cafenatedsql.api.driver.DialectDef;
 import net.witerat.cafenatedsql.api.driver.template.ExpressionFailedException;
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Interface DialectSelector.
  */
 public interface DialectSelector {
 
-	/**
-	 * Gets the dialect.
-	 *
-	 * @param model the model
-	 * @return the dialect
-	 */
-	DialectDef getDialect(TemplateEngineModel model);
+  /**
+   * Gets the dialect.
+   *
+   * @param model
+   *          the model
+   * @return the dialect
+   */
+  DialectDef getDialect(TemplateEngineModel model);
 
-	/**
-	 * Gets the dialect name.
-	 *
-	 * @param model the model
-	 * @return the dialect name
-	 * @throws ExpressionFailedException the expression failed exception
-	 */
-	String getDialectName(TemplateEngineModel model) throws ExpressionFailedException;
+  /**
+   * Gets the dialect name.
+   *
+   * @param model
+   *          the model
+   * @return the dialect name
+   * @throws ExpressionFailedException
+   *           the expression failed exception
+   */
+  String getDialectName(TemplateEngineModel model)
+      throws ExpressionFailedException;
 
 }
