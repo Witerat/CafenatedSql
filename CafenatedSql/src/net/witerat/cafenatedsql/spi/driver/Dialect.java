a0337adf5419e53ec9910090017cc763299d5586
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:47:30 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/Dialect.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/Dialect.java
@@ -7,50 +7,60 @@ import net.witerat.cafenatedsql.api.driver.GrammarDef;
 import net.witerat.cafenatedsql.api.driver.ParameterType;
 import net.witerat.cafenatedsql.api.driver.ParameterizedRequest;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class Dialect.
+ * The Class Dialect describes a specific language set for data definition
+ * and refactoring.
  */
 public class Dialect implements ParameterizedRequest, DialectDef {
 
-	/** The id. */
-	private String id;
-	
-	/** The grammars. */
-	LinkedHashMap<String, GrammarDef> grammars;
-	
-	/** The params. */
-	LinkedHashMap<String, ParameterType> params;
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.spi.driver.DialectDef#setName(java.lang.String)
-	 */
-	@Override
-	public void setName(String id) {
-		this.id=id;
-		
-	}
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.spi.driver.DialectDef#getId()
-	 */
-	@Override
-	public String getId() {
-		return id;
-	}
-	/**
-	 * Adds a grammar to this dialect
-	 * @see DialectDef#addGrammar(GrammarDef)
-	 */
-	@Override
-	public void addGrammar(GrammarDef g){
-		grammars.put(g.getName(), g);
-	}
-	/**
-	 * Adds a parameter to this <code>Dialect</code>.
-	 * @see DialectDef#add(ParameterType)
-	 */
-	@Override
-	public void add(ParameterType param){
-		params.put(param.getName(), param);
-	}
+  /** The id. */
+  private String id;
+
+  /** The grammars. */
+  private LinkedHashMap<String, GrammarDef> grammars;
+
+  /** The params. */
+  private LinkedHashMap<String, ParameterType> params;
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.spi.driver.DialectDef#setName(java.lang.String)
+   */
+  @Override
+  public void setName(final String id0) {
+    this.id = id0;
+
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.spi.driver.DialectDef#getId()
+   */
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * Adds a grammar to this dialect.
+   *
+   * @see DialectDef#addGrammar(GrammarDef)
+   */
+  @Override
+  public void addGrammar(final GrammarDef g) {
+    grammars.put(g.getName(), g);
+  }
+
+  /**
+   * Adds a parameter to this <code>Dialect</code>.
+   *
+   * @see DialectDef#add(ParameterType)
+   */
+  @Override
+  public void add(final ParameterType param) {
+    params.put(param.getName(), param);
+  }
 }
