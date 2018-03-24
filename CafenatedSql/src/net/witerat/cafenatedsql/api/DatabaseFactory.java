fe1b99a84b04f915e68665c89a7bdeabdcd692c3
John Hutcheson <witerat.test@gmail.com>
2018-03-24 15:33:31 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/api/DatabaseFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/DatabaseFactory.java
@@ -3,8 +3,23 @@ package net.witerat.cafenatedsql.api;
 import net.witerat.cafenatedsql.api.driver.DriverCreationException;
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 
+/**
+ * A contract for creating databases.
+ * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
+ *
+ */
 public interface DatabaseFactory extends CafenatedFactory {
 
-	Database newDatabase(TemplateEngineModel model) throws DriverCreationException;
+  /**
+   * Create a new database using the given model containing connection
+   * and DBMS language specification.
+   * @param model the connection model
+   * @return a new database that can facade the underling DBMS specified
+   * in the model.
+   * @throws DriverCreationException failed to create a database or
+   * configure driver.
+   */
+  Database newDatabase(TemplateEngineModel model)
+      throws DriverCreationException;
 
 }
