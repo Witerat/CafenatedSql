ed030041c0bc45a38028922e8e7da3267791957c
John Hutcheson <witerat.test@gmail.com>
2018-03-24 16:27:45 +1300

    JSLint and JavaDoc review.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/UrlTag.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/UrlTag.java
@@ -18,120 +18,143 @@ import net.witerat.cafenatedsql.spi.driver.Content;
 /**
  * The Class UrlTag.
  */
-@XmlType(name="UrlType",namespace="-//org.witerat/cafenated/sql")
+@XmlType(name = "UrlType", namespace = "-//org.witerat/cafenated/sql")
 public class UrlTag extends MacroTag {
-	
-	/** The macros. */
-	AbstractMacroLocator macros;
-	
-	/** The content. */
-	Collection<AbstractContent> content;
 
-	private LinkedList<Object> rawContent;
+  /** The macros. */
+  private AbstractMacroLocator macros;
 
-	private Map<String, ParameterTag> parametersByName;
+  /** The content. */
+  private Collection<AbstractContent> content;
 
-	/**
-	 * Sets the macros.
-	 *
-	 * @param macros the new macros
-	 */
-	public void setMacros(AbstractMacroLocator macros) {
-		this.macros = macros;
-	}
+  /**
+   * The rawContent property.
+   */
+  private LinkedList<Object> rawContent;
 
+  /**
+   * The parametersByName property.
+   */
+  private Map<String, ParameterTag> parametersByName;
 
-	/**
-	 * Gets the macros.
-	 *
-	 * @return the macros
-	 */
-	@XmlTransient
-	public AbstractMacroLocator getMacros(){
-		return macros;
-	}
+  /**
+   * Sets the macros.
+   *
+   * @param macros0
+   *          the new macros
+   */
+  public void setMacros(final AbstractMacroLocator macros0) {
+    this.macros = macros0;
+  }
 
+  /**
+   * Gets the macros.
+   *
+   * @return the macros
+   */
+  @XmlTransient
+  public AbstractMacroLocator getMacros() {
+    return macros;
+  }
 
-	/**
-	 * Adds the.
-	 *
-	 * @param content the content
-	 */
-	void add(AbstractContent content){
-		this.content.add(content);
-	}
+  /**
+   * {@inheritDoc}
+   * Adds the.
+   *
+   * @param content0
+   *          the content.
+   */
+  final void add(final AbstractContent content0) {
+    this.content.add(content0);
+  }
 
-	/**
-	 * @param raw
-	 */
-	@XmlElementRefs({@XmlElementRef(name="use",type=UseTag.class,namespace="-//org.witerat/cafenated/sql"),
-		@XmlElementRef(name="param",type=ParameterTag.class, namespace="-//org.witerat/cafenated/sql")})
-	@XmlMixed
-	public void setRawContent(List<Object> raw){
-		LinkedList<Object> rawC=new LinkedList<Object>();
-		for(Object o:raw){
-			if(o instanceof ParameterTag){
-				setParameter((ParameterTag)o);
-			}else {
-				rawC.add(o);
-			}
-		}
-		rawContent=rawC;
-	}
-	/**
-	 * @return a list of strings and tags strings and tags.
-	 */
-	public List<Object> getRawContent() {
-		if(rawContent==null)rawContent=new LinkedList<>();
-		return rawContent;
-	}
+  /**
+   * Add raw elements.
+   * @param raw A list of elements.
+   */
+  @XmlElementRefs({
+      @XmlElementRef(name = "use", type = UseTag.class,
+        namespace = "-//org.witerat/cafenated/sql"),
+      @XmlElementRef(name = "param", type = ParameterTag.class,
+        namespace = "-//org.witerat/cafenated/sql") })
+  @XmlMixed
+  public void setRawContent(final List<Object> raw) {
+    LinkedList<Object> rawC = new LinkedList<Object>();
+    for (Object o : raw) {
+      if (o instanceof ParameterTag) {
+        setParameter((ParameterTag) o);
+      } else {
+        rawC.add(o);
+      }
+    }
+    rawContent = rawC;
+  }
 
-	
-	@XmlTransient
-	private void setParameter(ParameterTag v) {
-		if(parametersByName==null) parametersByName=new LinkedHashMap<>();
-		parametersByName.put(v.getName(), v);
-	}
+  /**
+   * @return a list of strings and tags strings and tags.
+   */
+  public List<Object> getRawContent() {
+    if (rawContent == null) {
+      rawContent = new LinkedList<>();
+    }
+    return rawContent;
+  }
 
+  /**
+   * set a parameter definition.
+   * @param pt a parameter description.
+   */
+  @XmlTransient
+  private void setParameter(final ParameterTag pt) {
+    if (parametersByName == null) {
+      parametersByName = new LinkedHashMap<>();
+    }
+    parametersByName.put(pt.getName(), pt);
+  }
 
-	/**
-	 * Sets the content.
-	 *
-	 * @param content the new content
-	 */
-	public void setContent(Collection<AbstractContent> content) {
-		this.content = content;
-	}
+  /**
+   * Sets the content.
+   *
+   * @param content0
+   *          the new content
+   */
+  public void setContent(final Collection<AbstractContent> content0) {
+    this.content = content0;
+  }
 
+  /**
+   * Get content.
+   * @return a translation of raw into part processed content objects
+   *  that make up a template..
+   */
+  @XmlTransient
+  public Collection<AbstractContent> getContent() {
+    return content;
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.spi.driver.tags.MacroTag#getContent()
-	 */
-	@XmlTransient
-	public Collection<AbstractContent> getContent() {
-		return content;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.spi.driver.tags.MacroTag#getParent()
+   */
+  @XmlTransient
+  @Override
+  public AbstractContent getParent() {
+    return null;
+  }
 
-
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.spi.driver.tags.MacroTag#getParent()
-	 */
-	@XmlTransient
-	@Override
-	public AbstractContent getParent() {
-		return null;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.spi.driver.tags.MacroTag#getText(net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel)
-	 */
-	@Override
-	public String getText(TemplateEngineModel model) {
-		StringBuilder sb=new StringBuilder();
-		for(Content c:getContent()){
-			sb.append(c.getText(model));
-		}
-		return sb.toString();
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.spi.driver.tags.MacroTag#getText(net.witerat.
+   * cafenatedsql.api.driver.template.TemplateEngineModel)
+   */
+  @Override
+  public String getText(final TemplateEngineModel model0) {
+    StringBuilder sb = new StringBuilder();
+    for (Content c : getContent()) {
+      sb.append(c.getText(model0));
+    }
+    return sb.toString();
+  }
 }
