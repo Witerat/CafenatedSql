13742a62d8312261e51054dbf16847a9ace5e762
John Hutcheson <witerat.test@gmail.com>
2018-03-24 16:37:34 +1300

    JSLint review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/DriverCreationException.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/DriverCreationException.java
@@ -1,62 +1,69 @@
 package net.witerat.cafenatedsql.api.driver;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class DriverCreationException.
  */
 @SuppressWarnings("serial")
 public class DriverCreationException extends Exception {
 
-	/**
-	 * Instantiates a new driver creation exception.
-	 */
-	public DriverCreationException() {
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Instantiates a new driver creation exception.
+   */
+  public DriverCreationException() {
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * Instantiates a new driver creation exception.
-	 *
-	 * @param message the message
-	 */
-	public DriverCreationException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Instantiates a new driver creation exception.
+   *
+   * @param message
+   *          the message
+   */
+  public DriverCreationException(final String message) {
+    super(message);
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * Instantiates a new driver creation exception.
-	 *
-	 * @param cause the cause
-	 */
-	public DriverCreationException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Instantiates a new driver creation exception.
+   *
+   * @param cause
+   *          the cause
+   */
+  public DriverCreationException(final Throwable cause) {
+    super(cause);
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * Instantiates a new driver creation exception.
-	 *
-	 * @param message the message
-	 * @param cause the cause
-	 */
-	public DriverCreationException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Instantiates a new driver creation exception.
+   *
+   * @param message
+   *          the message
+   * @param cause
+   *          the cause
+   */
+  public DriverCreationException(final String message, final Throwable cause) {
+    super(message, cause);
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * Instantiates a new driver creation exception.
-	 *
-	 * @param message the message
-	 * @param cause the cause
-	 * @param enableSuppression the enable suppression
-	 * @param writableStackTrace the writable stack trace
-	 */
-	public DriverCreationException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Instantiates a new driver creation exception.
+   *
+   * @param message
+   *          the message
+   * @param cause
+   *          the cause
+   * @param enableSuppression
+   *          the enable suppression
+   * @param writableStackTrace
+   *          the writable stack trace
+   */
+  public DriverCreationException(final String message, final Throwable cause,
+      final boolean enableSuppression, final boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+    // TODO Auto-generated constructor stub
+  }
 
 }
