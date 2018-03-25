19929a6cfbb68c1d5ff736fda8c9b99a71918537
John Hutcheson <witerat.test@gmail.com>
2018-03-26 10:11:09 +1300

    JSLint revision

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/DriverLocator.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/DriverLocator.java
@@ -1,16 +1,15 @@
 package net.witerat.cafenatedsql.spi.driver;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Interface DriverLocator.
  */
 public interface DriverLocator {
 
-	/**
-	 * Gets the driver.
-	 *
-	 * @return the driver
-	 */
-	Driver getDriver();
+  /**
+   * Gets the driver.
+   *
+   * @return the driver
+   */
+  Driver getDriver();
 
 }
