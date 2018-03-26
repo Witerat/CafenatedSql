e168b8ac25ab2543ee4776261fd2bd5dd0b3b2fa
John Hutcheson <witerat.test@gmail.com>
2018-03-26 14:47:04 +1300

    JSLint review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/GrammarFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/GrammarFactory.java
@@ -1,20 +1,21 @@
 package net.witerat.cafenatedsql.api;
 
-import net.witerat.cafenatedsql.api.driver.GrammarDef;
 import net.witerat.cafenatedsql.api.driver.DialectDef;
+import net.witerat.cafenatedsql.api.driver.GrammarDef;
 
-// TODO: Auto-generated Javadoc
 /**
  * A factory contract for creating Grammar objects.
  */
 public interface GrammarFactory extends CafenatedFactory {
-	
-	/**
-	 * New grammar.
-	 *
-	 * @param dd the dd
-	 * @param name the name
-	 * @return A new grammar definition
-	 */
-	GrammarDef newGrammar(DialectDef dd, String name);
+
+  /**
+   * New grammar.
+   *
+   * @param dd
+   *          the dd
+   * @param name
+   *          the name
+   * @return A new grammar definition
+   */
+  GrammarDef newGrammar(DialectDef dd, String name);
 }
--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/ExpressionFailedException.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/ExpressionFailedException.java
@@ -1,59 +1,68 @@
 package net.witerat.cafenatedsql.api.driver.template;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class ExpressionFailedException.
+ * The Class ExpressionFailedException is thrown to indicate that an
+ * expression has failed to evaluate properly against a model.
  */
 public class ExpressionFailedException extends Exception {
 
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = 1L;
+  /** The Constant serialVersionUID. */
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Instantiates a new expression failed exception.
-	 *
-	 * @param msg the msg
-	 * @param cause the cause
-	 * @param enableSuppression the enable suppression
-	 * @param printableSackTrace the printable sack trace
-	 */
-	public ExpressionFailedException(String msg, Throwable cause, boolean enableSuppression, boolean printableSackTrace) {
-		super(msg, cause, enableSuppression, printableSackTrace);
-	}
+  /**
+   * Instantiates a new expression failed exception.
+   *
+   * @param msg
+   *          the msg
+   * @param cause
+   *          the cause
+   * @param enableSuppression
+   *          the enable suppression
+   * @param printableSackTrace
+   *          the printable sack trace
+   */
+  public ExpressionFailedException(final String msg, final Throwable cause,
+      final boolean enableSuppression, final boolean printableSackTrace) {
+    super(msg, cause, enableSuppression, printableSackTrace);
+  }
 
-	/**
-	 * Instantiates a new expression failed exception.
-	 *
-	 * @param msg the msg
-	 * @param cause the cause
-	 */
-	public ExpressionFailedException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
+  /**
+   * Instantiates a new expression failed exception.
+   *
+   * @param msg
+   *          the msg
+   * @param cause
+   *          the cause
+   */
+  public ExpressionFailedException(final String msg, final Throwable cause) {
+    super(msg, cause);
+  }
 
-	/**
-	 * Instantiates a new expression failed exception.
-	 *
-	 * @param msg the msg
-	 */
-	public ExpressionFailedException(String msg) {
-		super(msg);
-	}
+  /**
+   * Instantiates a new expression failed exception.
+   *
+   * @param msg
+   *          the msg
+   */
+  public ExpressionFailedException(final String msg) {
+    super(msg);
+  }
 
-	/**
-	 * Instantiates a new expression failed exception.
-	 *
-	 * @param cause the cause
-	 */
-	public ExpressionFailedException(Throwable cause) {
-		super(cause);
-	}
+  /**
+   * Instantiates a new expression failed exception.
+   *
+   * @param cause
+   *          the cause
+   */
+  public ExpressionFailedException(final Throwable cause) {
+    super(cause);
+  }
 
-	/**
-	 * Instantiates a new expression failed exception.
-	 */
-	public ExpressionFailedException() {
-		super();
-	}
+  /**
+   * Instantiates a new expression failed exception.
+   */
+  public ExpressionFailedException() {
+    super();
+  }
 
 }
