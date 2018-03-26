7c3770b786daa66a39dbba92dc72c2cc6cb2515d
John Hutcheson <witerat.test@gmail.com>
2018-03-26 14:48:21 +1300

    JSLint review and name property getter

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/GrammarTag.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/GrammarTag.java
@@ -10,84 +10,112 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
-import net.witerat.cafenatedsql.spi.driver.tags.NounTag;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class GrammarTag.
  */
-@XmlType(name="GrammarType",namespace="-//org.witerat/cafenated/sql")
+@XmlType(name = "GrammarType", namespace = "-//org.witerat/cafenated/sql")
 public class GrammarTag {
-	
-	/** The name. */
-	String name;
-	
-	/** The nouns. */
-	Collection<NounTag> nouns;
-	
-	/** The params. */
-	Map<String, ParameterTag> params;
 
-	/**
-	 * Sets the noun.
-	 *
-	 * @param noun the new noun
-	 */
-	@XmlElement(name="noun", type=NounTag.class, namespace="-//org.witerat/cafenated/sql")
-	void setNoun(NounTag noun) {
-		if(nouns==null)nouns=new ArrayList<>();
-		nouns.add(noun);
-	}
+  /** The name. */
+  private String name;
 
-	/**
-	 * Gets the nouns.
-	 *
-	 * @return the nouns
-	 */
-	@XmlTransient
-	public Collection<NounTag> getNouns() {
-		if(nouns==null)nouns=new ArrayList<>();
-		return nouns;
-	}
+  /** The nouns. */
+  private Collection<NounTag> nouns;
 
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
-	@XmlAttribute(name="name")
-	public String getId() {
-		return name;
-	}
+  /** The params. */
+  private Map<String, ParameterTag> params;
 
-	/**
-	 * Sets the id.
-	 *
-	 * @param name the new id
-	 */
-	public void setId(String name) {
-		this.name = name;
-	}
+  /**
+   * Sets the noun.
+   *
+   * @param noun
+   *          the new noun
+   */
+  @XmlElement(name = "noun", type = NounTag.class,
+      namespace = "-//org.witerat/cafenated/sql")
+  void setNoun(final NounTag noun) {
+    if (nouns == null) {
+      nouns = new ArrayList<>();
+    }
+    nouns.add(noun);
+  }
 
-	/**
-	 * Gets the params.
-	 *
-	 * @return the params
-	 */
-	@XmlTransient
-	public Collection<ParameterTag> getParams() {
-		if(params==null)params=new LinkedHashMap<>();
-		return params.values();
-	}
-	
-	/**
-	 * Sets the parameter.
-	 *
-	 * @param p the new parameter
-	 */
-	@XmlElement(name="param", type=ParameterTag.class,namespace="-//org.witerat/cafenated/sql")
-	void setParameter(ParameterTag p){
-		if(params==null)params=new LinkedHashMap<>();
-		params.put(p.getName(), p);
-	}
+  /**
+   * Gets the nouns.
+   *
+   * @return the nouns
+   */
+  @XmlTransient
+  public Collection<NounTag> getNouns() {
+    if (nouns == null) {
+      nouns = new ArrayList<>();
+    }
+    return nouns;
+  }
+
+  /**
+   * Gets the id.
+   *
+   * @return the id
+   */
+  @XmlAttribute(name = "name")
+  public String getId() {
+    return name;
+  }
+
+  /**
+   * Sets the id.
+   *
+   * @param name0
+   *          the new id
+   */
+  public void setId(final String name0) {
+    this.name = name0;
+  }
+
+  /**
+   * Gets the params.
+   *
+   * @return the params
+   */
+  @XmlTransient
+  public Collection<ParameterTag> getParams() {
+    if (params == null) {
+      params = new LinkedHashMap<>();
+    }
+    return params.values();
+  }
+
+  /**
+   * Sets the parameter.
+   *
+   * @param p
+   *          the new parameter
+   */
+  @XmlElement(name = "param", type = ParameterTag.class,
+      namespace = "-//org.witerat/cafenated/sql")
+  void setParameter(final ParameterTag p) {
+    if (params == null) {
+      params = new LinkedHashMap<>();
+    }
+    params.put(p.getName(), p);
+  }
+
+  /**
+   * Gets the name of this Grammar.
+   * @return the name of this grammar.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Defines the name of this Grammar.
+   * @param name0 the new name of this grammar.
+   */
+  @XmlAttribute(name = "name")
+  public void setName(final String name0) {
+    this.name = name0;
+  }
 }
