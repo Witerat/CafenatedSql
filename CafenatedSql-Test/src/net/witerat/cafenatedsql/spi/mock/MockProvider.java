b9b78a235b4133508b2d8866d65991be9ce1e9d0
John Hutcheson <witerat.test@gmail.com>
2018-03-26 21:12:01 +1300

    JSLint review

--- CafenatedSql-Test/src/net/witerat/cafenatedsql/spi/mock/MockProvider.java
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/spi/mock/MockProvider.java
@@ -1,6 +1,5 @@
 package net.witerat.cafenatedsql.spi.mock;
 
-import java.awt.color.CMMException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,150 +20,153 @@ import net.witerat.cafenatedsql.api.ViewFactory;
 
 public class MockProvider implements Provider {
 
-	private Map<String, Object> properties;
-	private String[] connectionMethods={"mock"};
-	private String mockDriverClass="MockDriver";
-	private DatabaseFactory databaseFactory;
+  private Map<String, Object> properties;
+  private String[] connectionMethods = { "mock" };
+  private String mockDriverClass = "MockDriver";
+  private DatabaseFactory databaseFactory;
 
-	@Override
-	public ConnectionFactory getConnectionFactory() {
-		return getConnectionFactory(connectionMethods[0]);
-	}
+  @Override
+  public ConnectionFactory getConnectionFactory() {
+    return getConnectionFactory(connectionMethods[0]);
+  }
 
-	@Override
-	public String[] getConnectionMethods() {
-		return connectionMethods;
-	}
+  @Override
+  public ConnectionFactory getConnectionFactory(String method) {
+    if (connectionFactories.containsKey(method)) {
+      return connectionFactories.get(method);
+    }
+    MockConnectionFactory mcf = new MockConnectionFactory(properties);
+    mcf.properties = properties;
+    return mcf;
+  }
 
-	@Override
-	public ConnectionFactory getConnectionFactory(String method) {
-		if(connectionFactories.containsKey(method))
-			return connectionFactories.get(method);
-		MockConnectionFactory mcf = new MockConnectionFactory(properties);
-		mcf.properties=properties;
-		return mcf;
-	}
+  @Override
+  public String[] getConnectionMethods() {
+    return connectionMethods;
+  }
 
-	@Override
-	public DialectFactory getDialectFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public DialectFactory getDialectFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public String getDriverClass() {
-		// TODO Auto-generated method stub
-		return mockDriverClass;
-	}
+  @Override
+  public String getDriverClass() {
+    // TODO Auto-generated method stub
+    return mockDriverClass;
+  }
 
-	@Override
-	public DatabaseFactory getDatabaseFactory() {
-		// TODO Auto-generated method stub
-		return databaseFactory;
-	}
+  @Override
+  public DatabaseFactory getDatabaseFactory() {
+    // TODO Auto-generated method stub
+    return databaseFactory;
+  }
 
-	@Override
-	public GrammarFactory getGrammarFactory() {
-		// TODO Auto-generated method stub
-		return (GrammarFactory)getDriverFactory();
-	}
+  @Override
+  public GrammarFactory getGrammarFactory() {
+    // TODO Auto-generated method stub
+    return (GrammarFactory) getDriverFactory();
+  }
 
-	@Override
-	public NounFactory getNounFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public NounFactory getNounFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public ParameterFactory getParameterFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public ParameterFactory getParameterFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public SchemaFactory getSchemaFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public SchemaFactory getSchemaFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public TableFactory getTableFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public TableFactory getTableFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public URLFactory getUrlFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public URLFactory getUrlFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public VerbFactory getVerbFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public VerbFactory getVerbFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public ViewFactory getViewFactory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	Map<String, ConnectionFactory > connectionFactories=new LinkedHashMap<>();
- 	public void setConnectionFactory(ConnectionFactory mcf) {
-		connectionFactories.put((String) mcf.getProperty("connection_method"), mcf);
-	}
+  @Override
+  public ViewFactory getViewFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	public void setTemplateModelFactory(TemplateModelFactory templateModelFactory) {
-	}
+  Map<String, ConnectionFactory> connectionFactories = new LinkedHashMap<>();
 
-	public TemplateModelFactory getTemplateModelFactory() {
-		return null;
-	}
+  public void setConnectionFactory(ConnectionFactory mcf) {
+    connectionFactories.put((String) mcf.getProperty("connection_method"), mcf);
+  }
 
-	public void setViewFactory(ViewFactory viewFactory) {
-	}
+  public void setTemplateModelFactory(TemplateModelFactory templateModelFactory) {
+  }
 
-	public void setVerbFactory(VerbFactory verbFactory) {
-	}
+  public TemplateModelFactory getTemplateModelFactory() {
+    return null;
+  }
 
-	public void setUrlFactory(URLFactory urlFactory) {
-	}
+  public void setViewFactory(ViewFactory viewFactory) {
+  }
 
-	public void setTableFactory(TableFactory tableFactory) {
-	}
+  public void setVerbFactory(VerbFactory verbFactory) {
+  }
 
-	public void setSchemaFactory(SchemaFactory schemaFactory) {
-	}
+  public void setUrlFactory(URLFactory urlFactory) {
+  }
 
-	public void setParamaterFactory(ParameterFactory paramaterFactory) {
-	}
+  public void setTableFactory(TableFactory tableFactory) {
+  }
 
-	public void setNounFactory(NounFactory nounFactory) {
-	}
+  public void setSchemaFactory(SchemaFactory schemaFactory) {
+  }
 
-	public void setGrammarFactory(GrammarFactory grammarFactory) {
-	}
+  public void setParamaterFactory(ParameterFactory paramaterFactory) {
+  }
 
-	public void setDriverFactory(DatabaseFactory driverFactory) {
-	}
+  public void setNounFactory(NounFactory nounFactory) {
+  }
 
-	public void setDriverClass(String driverClass) {
-	}
+  public void setGrammarFactory(GrammarFactory grammarFactory) {
+  }
 
-	public void setDialectFactory(DialectFactory dialectFactory) {
-	}
+  public void setDriverFactory(DatabaseFactory driverFactory) {
+  }
 
-	@Override
-	public DriverFactory getDriverFactory() {
-		// TODO Auto-generated method stub
-		return new MockDriverFactory();
-	}
+  public void setDriverClass(String driverClass) {
+  }
 
-	@Override
-	public TemplateModelFactory getModelFactory() {
-		// TODO Auto-generated method stub
-		return new MockModelFactory();
-	}
+  public void setDialectFactory(DialectFactory dialectFactory) {
+  }
+
+  @Override
+  public DriverFactory getDriverFactory() {
+    // TODO Auto-generated method stub
+    return new MockDriverFactory();
+  }
+
+  @Override
+  public TemplateModelFactory getModelFactory() {
+    // TODO Auto-generated method stub
+    return new MockModelFactory();
+  }
 
 }
