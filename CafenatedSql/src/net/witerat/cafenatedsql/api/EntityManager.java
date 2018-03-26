f609fd7622c81db55f372834ed45a5c35e853f06
John Hutcheson <witerat.test@gmail.com>
2018-03-26 14:35:57 +1300

    Initial commit
    Value and operations on database entity.

---
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/EntityManager.java
@@ -0,0 +1,34 @@
+package net.witerat.cafenatedsql.api;
+
+/**
+ * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
+ *
+ */
+public interface EntityManager {
+
+  /**
+   * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
+   *
+   */
+  enum Calculus {
+    /**
+     * The CREATE property.
+     */
+    CREATE,
+    /**
+     * The DROP property.
+     */
+    DROP,
+    /**
+     * The ALTER property.
+     */
+    ALTER
+  }
+  /**
+   * @param noun Dictionary context of operation
+   * @param operation the operation
+   * @param obj target of operation.
+   */
+  void refactor(String noun, Calculus operation, Object obj);
+
+}
