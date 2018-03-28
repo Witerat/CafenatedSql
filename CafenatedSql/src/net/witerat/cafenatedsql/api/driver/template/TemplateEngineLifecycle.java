8e917978b619cb7bf9b8ffac0bd964ed68559e41
John Hutcheson <witerat.test@gmail.com>
2018-03-28 22:06:48 +1300

    JSLint and javaDoc review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/TemplateEngineLifecycle.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/TemplateEngineLifecycle.java
@@ -1,43 +1,42 @@
 package net.witerat.cafenatedsql.api.driver.template;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Interface TemplateEngineLifecycle.
  */
 public interface TemplateEngineLifecycle {
 
-	/**
-	 * An event to advise resources are loaded.
-	 */
-	void onLoad();
+  /**
+   * An event to advise resources are loaded.
+   */
+  void onLoad();
 
-	/**
-	 * an event to advise that an underling template service is about to be
-	 * created.
-	 */
-	void beforeNew();
+  /**
+   * an event to advise that an underling template service is about to be
+   * created.
+   */
+  void beforeNew();
 
-	/**
-	 * New engine.
-	 *
-	 * @return the template engine
-	 */
-	TemplateEngine newEngine();
+  /**
+   * New engine.
+   *
+   * @return the template engine
+   */
+  TemplateEngine newEngine();
 
-	/**
-	 * A temmplate service object is about to be discarded
-	 */
-	void cleanup();
+  /**
+   * A template service object is about to be discarded.
+   */
+  void cleanup();
 
-	/**
-	 * Dispose of the template service object.
-	 */
-	void dispose();
+  /**
+   * Dispose of the template service object.
+   */
+  void dispose();
 
-	/**
-	 * cafenated SQL or the JVM on which it runs is shutting down. Time to
-	 * release resource in an orderly fashion and quickly.
-	 */
-	void shutdown();
+  /**
+   * cafenated SQL or the JVM on which it runs is shutting down. Time to release
+   * resource in an orderly fashion and quickly.
+   */
+  void shutdown();
 
 }
--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/TemplateEngineModel.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/TemplateEngineModel.java
@@ -1,54 +1,57 @@
 package net.witerat.cafenatedsql.api.driver.template;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Interface TemplateEngineModel.
+ * The Interface TemplateEngineModel encapsulates a contract for
+ * implementing a model usual by employing the model of the the
+ * supporting templating component.
  */
 public interface TemplateEngineModel {
 
-	/**
-	 * gets the value of a model property by evaluating the expression language
-	 * of the underlying template system. Often a convenience method that calls
-	 * {@link #evaluateContextExpression(String)}.
-	 * 
-	 * @param expression
-	 *            an expression in the template system's native expression
-	 *            language;
-	 * @return the evaluation product for expression, or null if the evaluation
-	 *         fails.
-	 */
-	Object getByExpression(String expression);
+  /**
+   * gets the value of a model property by evaluating the expression language of
+   * the underlying template system. Often a convenience method that calls
+   * {@link #evaluateContextExpression(String)}.
+   *
+   * @param expression
+   *          an expression in the template system's native expression language;
+   * @return the evaluation product for expression, or null if the evaluation
+   *         fails.
+   */
+  Object getByExpression(String expression);
 
-	/**
-	 * gets the value of the specified property from the backing model.
-	 * 
-	 * @param property
-	 *            the property name(<code>[a-z)[a-zA-Z0-9]*</code>) or property
-	 *            expression(<code>[a-z)[a-zA-Z0-9]*{.[a-z)[a-zA-Z0-9]*}*</code>)
-	 *            of the property to retrieve.
-	 * @return the value of the property.
-	 */
-	Object get(String property);
+  /**
+   * gets the value of the specified property from the backing model.
+   *
+   * @param property
+   *          the property name(<code>[a-z)[a-zA-Z0-9]*</code>) or property
+   *          expression(<code>[a-z)[a-zA-Z0-9]*{.[a-z)[a-zA-Z0-9]*}*</code>) of
+   *          the property to retrieve.
+   * @return the value of the property.
+   */
+  Object get(String property);
 
-	/**
-	 * Evaluate context expression.
-	 *
-	 * @param expression            text representing an evaluable expression for the underlying
-	 *            template system.
-	 * @return the evaluated result of the expression.
-	 * @throws ExpressionFailedException             The template system failed to evaluate the expression and
-	 *             arrive at a result.
-	 */
-	Object evaluateContextExpression(String expression) throws ExpressionFailedException;
+  /**
+   * Evaluate context expression.
+   *
+   * @param expression
+   *          text representing an evaluable expression for the underlying
+   *          template system.
+   * @return the evaluated result of the expression.
+   * @throws ExpressionFailedException
+   *           The template system failed to evaluate the expression and arrive
+   *           at a result.
+   */
+  Object evaluateContextExpression(String expression)
+      throws ExpressionFailedException;
 
-	/**
-	 * Set a model property.
-	 * 
-	 * @param property
-	 *            the property name or expression
-	 * @param value
-	 *            the new value of the property
-	 */
-	void set(String property, Object value);
+  /**
+   * Set a model property.
+   *
+   * @param property
+   *          the property name or expression
+   * @param value
+   *          the new value of the property
+   */
+  void set(String property, Object value);
 
 }
