86c615997d3fbe2cfb2023509f17ec015b1d24da
John Hutcheson <witerat.test@gmail.com>
2018-03-28 22:20:43 +1300

    JSLint and javaDoc review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/Provider.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/Provider.java
@@ -1,103 +1,118 @@
 package net.witerat.cafenatedsql.api;
 
-import net.witerat.cafenatedsql.api.DialectFactory;
-import net.witerat.cafenatedsql.api.NounFactory;
-import net.witerat.cafenatedsql.api.VerbFactory;
 
 /**
  * The Interface Provider.
  */
 public interface Provider {
-	
-	/**
-	 * Gets the connection factory.
-	 *
-	 * @return the connection factory
-	 */
-	public abstract ConnectionFactory	getConnectionFactory();
-	
-	/**
-	 * Gets the schema factory.
-	 *
-	 * @return the schema factory
-	 */
-	public abstract SchemaFactory getSchemaFactory();
-	
-	/**
-	 * Gets the table factory.
-	 *
-	 * @return the table factory
-	 */
-	public abstract TableFactory getTableFactory(); 
-	
-	/**
-	 * Gets the view factory.
-	 *
-	 * @return the view factory
-	 */
-	public abstract ViewFactory getViewFactory(); 
-	
-	/**
-	 * Gets the driver factory.
-	 *
-	 * @return the driver factory
-	 */
-	public abstract DriverFactory getDriverFactory();
-	
-	/**
-	 * Gets the driver class.
-	 *
-	 * @return the driver class
-	 */
-	public abstract String getDriverClass();
-	
-	/**
-	 * Gets the grammar factory.
-	 *
-	 * @return the grammar factory
-	 */
-	public abstract GrammarFactory getGrammarFactory();
-	
-	/**
-	 * Gets the url factory.
-	 *
-	 * @return the url factory
-	 */
-	public abstract URLFactory getUrlFactory();
-	
-	/**
-	 * Gets the parameter factory.
-	 *
-	 * @return the parameter factory
-	 */
-	public abstract ParameterFactory getParameterFactory();
-	
-	/**
-	 * Gets the verb factory.
-	 *
-	 * @return the verb factory
-	 */
-	public abstract VerbFactory getVerbFactory();
-	
-	/**
-	 * Gets the noun factory.
-	 *
-	 * @return the noun factory
-	 */
-	public abstract NounFactory getNounFactory();
-	
-	/**
-	 * Gets the dialect factory.
-	 *
-	 * @return the dialect factory
-	 */
-	public abstract DialectFactory getDialectFactory();
 
-	String[] getConnectionMethods();
+  /**
+   * Gets the connection factory.
+   *
+   * @return the connection factory
+   */
+  ConnectionFactory getConnectionFactory();
 
-	ConnectionFactory getConnectionFactory(String method);
+  /**
+   * Get a connection factory suitable the specified method.
+   * @param method the method of connection
+   * @return a factory for connecting by the specified method
+   */
+  ConnectionFactory getConnectionFactory(String method);
 
-	DatabaseFactory getDatabaseFactory();
+  /**
+   * Gets the available connection methods.
+   * @return an array of connection method names
+   */
+  String[] getConnectionMethods();
 
-	TemplateModelFactory getModelFactory();
+  /**
+   * Get a database factory the configures database access for this provider.
+   * @return a database factory
+   */
+  DatabaseFactory getDatabaseFactory();
+
+  /**
+   * Gets the dialect factory.
+   *
+   * @return the dialect factory
+   */
+  DialectFactory getDialectFactory();
+
+  /**
+   * Gets the driver class.
+   *
+   * @return the driver class
+   */
+  String getDriverClass();
+
+  /**
+   * Gets the driver factory.
+   *
+   * @return the driver factory
+   */
+  DriverFactory getDriverFactory();
+
+  /**
+   * Gets the grammar factory.
+   *
+   * @return the grammar factory
+   */
+  GrammarFactory getGrammarFactory();
+
+  /**
+   * This property refers to a model factory creates models for use with
+   * the underlying templat engine.
+   * @return A model for passing parameter information to DBMS.
+   */
+  TemplateModelFactory getModelFactory();
+
+  /**
+   * Gets the noun factory.
+   *
+   * @return the noun factory
+   */
+  NounFactory getNounFactory();
+
+  /**
+   * Gets the parameter factory.
+   *
+   * @return the parameter factory
+   */
+  ParameterFactory getParameterFactory();
+
+  /**
+   * Gets the schema factory.
+   *
+   * @return the schema factory
+   */
+  SchemaFactory getSchemaFactory();
+
+  /**
+   * Gets the table factory.
+   *
+   * @return the table factory
+   */
+  TableFactory getTableFactory();
+
+  /**
+   * Gets the url factory.
+   *
+   * @return the url factory
+   */
+  URLFactory getUrlFactory();
+
+  /**
+   * Gets the verb factory.
+   *
+   * @return the verb factory
+   */
+  VerbFactory getVerbFactory();
+
+  /**
+   * Gets the view factory.
+   *
+   * @return the view factory
+   */
+  ViewFactory getViewFactory();
 }
