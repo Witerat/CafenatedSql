99d8e17868174dcacfdb0f7d3caa5e627e31bfc8
John Hutcheson <witerat.test@gmail.com>
2018-03-29 14:18:05 +1300

    JSLint review

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/ResourceProtocol.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/ResourceProtocol.java
@@ -9,56 +9,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The Class ResourceProtocol provide late registration for URL protocol handlers..
+ * The Class ResourceProtocol provide late registration for URL protocol
+ * handlers..
  *
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
  */
 public class ResourceProtocol implements URLStreamHandlerFactory {
-	
-	/** The Constant protocolHandlers. */
-	private static final Map<String, URLStreamHandler> protocolHandlers = new HashMap<String, URLStreamHandler>();
-	static {
-		protocolHandlers.put("classpath", new URLStreamHandler() {
 
-			@Override
-			protected URLConnection openConnection(URL u) throws IOException {
-				return Thread.currentThread().getContextClassLoader().getResource(u.getPath()).openConnection();
-			}
-		});
-	}
-	static {
-		System.out.println("registering classpath");
-		URL.setURLStreamHandlerFactory(new ResourceProtocol());
-		System.out.println("registered classpath successfully");
-	}
+  /** The Constant protocolHandlers. */
+  private static final Map<String, URLStreamHandler> PROTOCOL_HANDLERS =
+      new HashMap<String, URLStreamHandler>();
 
-	/**
-	 * Register protocol.
-	 *
-	 * @param protocol the protocol
-	 * @param handler the handler
-	 */
-	public void registerProtocol(String protocol, URLStreamHandler handler) {
-		protocolHandlers.put(protocol, handler);
-	}
+  static {
+    PROTOCOL_HANDLERS.put("classpath", new URLStreamHandler() {
 
-	/* (non-Javadoc)
-	 * @see java.net.URLStreamHandlerFactory#createURLStreamHandler(java.lang.String)
-	 */
-	@Override
-	public URLStreamHandler createURLStreamHandler(String protocol) {
+      @Override
+      protected URLConnection openConnection(final URL u) throws IOException {
+        Thread ct = Thread.currentThread();
+        ClassLoader contextCL = ct.getContextClassLoader();
+        return contextCL.getResource(u.getPath()).openConnection();
+      }
+    });
+  }
 
-		return protocolHandlers.get(protocol);
-	}
+  static {
+    System.out.println("registering classpath");
+    URL.setURLStreamHandlerFactory(new ResourceProtocol());
+    System.out.println("registered classpath successfully");
+  }
 
-	/**
-	 * Initialises this class. Invocation of this method forces the class to be loaded and initialise.
-	 *
-	 * @return return the number of default handlers pre-registered
-	 */
-	public static int init() {
-		return protocolHandlers.size();
+  /**
+   * Register protocol.
+   *
+   * @param protocol
+   *          the protocol
+   * @param handler
+   *          the handler
+   */
+  public void registerProtocol(final String protocol,
+      final URLStreamHandler handler) {
+    PROTOCOL_HANDLERS.put(protocol, handler);
+  }
 
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * java.net.URLStreamHandlerFactory#createURLStreamHandler(java.lang.String)
+   */
+  @Override
+  public URLStreamHandler createURLStreamHandler(final String protocol) {
+
+    return PROTOCOL_HANDLERS.get(protocol);
+  }
+
+  /**
+   * Initialises this class. Invocation of this method forces the class to be
+   * loaded and initialise.
+   *
+   * @return return the number of default handlers pre-registered
+   */
+  public static int init() {
+    return PROTOCOL_HANDLERS.size();
+
+  }
 
 }
