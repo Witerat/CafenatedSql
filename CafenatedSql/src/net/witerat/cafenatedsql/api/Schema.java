2a6043fceaa77fa722d339cbd279baa8c006b077
John Hutcheson <witerat.test@gmail.com>
2018-03-29 14:26:39 +1300

    JSLint and javaDoc review

--- CafenatedSql/src/net/witerat/cafenatedsql/api/Schema.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/Schema.java
@@ -2,25 +2,31 @@ package net.witerat.cafenatedsql.api;
 
 import java.util.List;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Interface Schema.
  */
 public interface Schema {
-	
-	/**
-	 * Gets the name.
-	 *
-	 * @return the name
-	 */
-	String getName();
-	
-	/**
-	 * Sets the name.
-	 *
-	 * @param name the new name
-	 */
-	void setName(String name);
 
-	void setJournal(List<Refactor> refactors);
+  /**
+   * Gets the name.
+   *
+   * @return the name
+   */
+  String getName();
+
+  /**
+   * Sets the name.
+   *
+   * @param name
+   *          the new name
+   */
+  void setName(String name);
+
+  /**
+   * @param refactors a list modifications
+   */
+  void setJournal(List<Refactor> refactors);
+
+  /** Save changes. */
+  void update();
 }
--- CafenatedSql/src/net/witerat/cafenatedsql/api/SchemaFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/SchemaFactory.java
@@ -1,26 +1,32 @@
 package net.witerat.cafenatedsql.api;
 
-// TODO: Auto-generated Javadoc
 /**
- * 
  * Encapsulates access to new and existing schemas.
  *
  */
 
 public interface SchemaFactory extends CafenatedFactory {
-	
-	/**
-	 *  gets a named schema from the data base .
-	 *
-	 * @param name the name of the schema to get.
-	 * @return the schema having the specified name.
-	 */
-	Schema getSchema(String name);
 
-	/**
-	 *  creates a new schema reference .
-	 *
-	 * @return a new schema
-	 */
-	Schema createSchema();
+  /**
+   * gets a named schema from the data base .
+   *
+   * @param name
+   *          the name of the schema to get.
+   * @return the schema having the specified name.
+   */
+  Schema getSchema(String name);
+
+  /**
+   * creates a new schema reference .
+   *
+   * @return a new schema
+   */
+  Schema createSchema();
+
+  /**
+   * Instruct the underlying db to to drop a schema.
+   * @param name name of the schema to drop.
+   * @return A named empty schema.
+   */
+  Schema dropSchema(String name);
 }
