3c5a4157b617953a3baa0da72733fea9b8ffb612
John Hutcheson <witerat.test@gmail.com>
2018-03-29 14:21:10 +1300

    JSLint and javaDoc review

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/RootProviderRegistrar.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/RootProviderRegistrar.java
@@ -1,6 +1,5 @@
 package net.witerat.cafenatedsql.spi;
 
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -14,83 +13,128 @@ import net.witerat.cafenatedsql.api.ProviderRegistrar;
  *
  */
 public class RootProviderRegistrar extends SimpleProviderRegistrar {
-	private Map<String, ProviderRegistrar> registrars = new LinkedHashMap();
-	private Set<ProviderRegistrar> anons = new LinkedHashSet<>();
+  /**
+   * The registrars property.
+   */
+  private Map<String, ProviderRegistrar> registrars =
+      new LinkedHashMap<>();
+  /**
+   * The anons property.
+   */
+  private Set<ProviderRegistrar> anons = new LinkedHashSet<>();
 
-	void addRegistrar(ProviderRegistrar pr) {
-		registrars.put(getDefaultName(pr), pr);
-		anons.add(pr);
-	}
+  /**
+   * Add an unnamed registrar.
+   * @param pr a registrar.
+   */
+  public void addRegistrar(final ProviderRegistrar pr) {
+    registrars.put(getDefaultName(pr), pr);
+    anons.add(pr);
+  }
 
-	public void addRegistrar(String name, ProviderRegistrar pr) {
-		int d = name.indexOf("/");
-		if (d == -1) {
-			registrars.put(name, pr);
-		} else {
-			((RootProviderRegistrar) registrars.get(name.substring(0, d))).addRegistrar(name.substring(d + 1), pr);
-		}
-	}
+  /**
+   * Adds a named registrar.
+   *
+   * @param name A registration for the provider
+   * @param pr the provider.
+   */
+  public void addRegistrar(final String name, final ProviderRegistrar pr) {
+    int d = name.indexOf("/");
+    if (d == -1) {
+      registrars.put(name, pr);
+    } else {
+      ((RootProviderRegistrar) registrars.get(name.substring(0, d)))
+      .addRegistrar(name.substring(d + 1), pr);
+    }
+  }
 
-	/**
-	 * @param pr
-	 * @return a name used by anonymous registrars.
-	 */
-	public static String getDefaultName(ProviderRegistrar pr) {
-		return "{@" + System.identityHashCode(pr) + "}";
-	}
+  /**
+   * Creates a name for an anonymous registrar.
+   *
+   * @param pr the provider.
+   * @return a name used by anonymous registrars.
+   */
+  public static String getDefaultName(final ProviderRegistrar pr) {
+    return "{@" + System.identityHashCode(pr) + "}";
+  }
 
-	/**
-	 * remove the specified registrar.
-	 * @param pr
-	 */
-	void removeRegistrar(ProviderRegistrar pr) {
-		while(registrars.values().remove(pr));
-		while(anons.remove(pr));
-	}
+  /**
+   * Remove the specified registrar.
+   *
+   * @param pr The provider registrar.
+   * @see RootProviderRegistrar#addRegistrar(ProviderRegistrar)
+   */
+  public void removeRegistrar(final ProviderRegistrar pr) {
+    while (registrars.values().remove(pr)) {
+      continue;
+    }
+    while (anons.remove(pr)) {
+      continue;
+    }
+  }
 
-	/**
-	 * Removes the named registrar
-	 * @param name the name of the registrar to be removed.
-	 */
-	public void removeRegistrar(String name) {
-		registrars.remove(name);
-	}
+  /**
+   * Removes the named registrar.
+   *
+   * @param name
+   *          the name of the registrar to be removed.
+   */
+  public void removeRegistrar(final String name) {
+    registrars.remove(name);
+  }
 
-	@Override
-	public String getDescription(String pathName) {
-		String[] path = pathName.split("/");
-		RootProviderRegistrar rpr = this;
-		ProviderRegistrar pr = this;
-		for (int i = 0; i < path.length - 1; i++) {
-			rpr = (RootProviderRegistrar) pr;
-			pr = rpr.registrars.get(path[i]);
-		}
-		if (pr == this)
-			return super.getDescription(path[path.length - 1]);
-		return pr.getDescription(path[path.length - 1]);
-	}
-	@Override
-	public Provider getProvider(String name) {
-		if(name.indexOf('/')==-1){
-			Provider p = super.getProvider(name);
-			if(p==null){
-				for(ProviderRegistrar pr: anons){
-					p=pr.getProvider(name);
-					if(p!=null) break;
-				}
-			}
-			return p;
-		}else{
-			String[] path=name.split("/");
-			RootProviderRegistrar rpr=this;
-			ProviderRegistrar pr=this;
-			for (int i = 0; i < path.length - 1; i++) {
-				rpr = (RootProviderRegistrar) pr;
-				pr = rpr.registrars.get(path[i]);
-			}
-			return pr==this?super.providers.get(path[path.length-1])
-					:pr.getProvider(path[path.length-1]);
-			
-		}
-	}
+  /**
+   * {@inheritDoc}
+   * @see net.witerat.cafenatedsql.spi.SimpleProviderRegistrar
+   *    #getDescription(java.lang.String)
+   */
+  @Override
+  public String getDescription(final String pathName) {
+    String[] path = pathName.split("/");
+    RootProviderRegistrar rpr = this;
+    ProviderRegistrar pr = this;
+    for (int i = 0; i < path.length - 1; i++) {
+      rpr = (RootProviderRegistrar) pr;
+      pr = rpr.registrars.get(path[i]);
+    }
+    if (pr == this) {
+      return super.getDescription(path[path.length - 1]);
+    }
+    return pr.getDescription(path[path.length - 1]);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see net.witerat.cafenatedsql.spi.SimpleProviderRegistrar
+   *    #getProvider(java.lang.String)
+   */
+  @Override
+  public Provider getProvider(final String name) {
+    if (name.indexOf('/') == -1) {
+      Provider p = super.getProvider(name);
+      if (p == null) {
+        for (ProviderRegistrar pr : anons) {
+          p = pr.getProvider(name);
+          if (p != null) {
+            break;
+          }
+        }
+      }
+      return p;
+    } else {
+      String[] path = name.split("/");
+      RootProviderRegistrar rpr = this;
+      ProviderRegistrar pr = this;
+      for (int i = 0; i < path.length - 1; i++) {
+        rpr = (RootProviderRegistrar) pr;
+        pr = rpr.registrars.get(path[i]);
+      }
+      if (pr == this) {
+        return  super.getProviders().get(path[path.length - 1]);
+      } else {
+        return pr.getProvider(path[path.length - 1]);
+      }
+
+    }
+  }
 }
