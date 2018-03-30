9dba77414c783befc93d338fe33b87d76f6dd16d
John Hutcheson <witerat.test@gmail.com>
2018-03-30 21:22:05 +1300

    JSLint and JavaDoc review

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/SimpleConnectionType.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/SimpleConnectionType.java
@@ -3,71 +3,86 @@ package net.witerat.cafenatedsql.spi.driver;
 import net.witerat.cafenatedsql.api.driver.ConnectionType;
 import net.witerat.cafenatedsql.api.driver.UrlDef;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class SimpleConnectionType.
  */
 public class SimpleConnectionType implements ConnectionType {
-	
-	/** The dialect selector. */
-	DialectSelector dialectSelector;
-	
-	/** The name. */
-	String name;
 
-	/** The description. */
-	String description;
+  /** The dialect selector. */
+  private DialectSelector dialectSelector;
 
+  /** The name. */
+  private String name;
 
-	/** The url def. */
-	private UrlDef urlDef;
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getDescription()
-	 */
-	@Override
-	public String getDescription() {
-		return description;
-	}
+  /** The description. */
+  private String description;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getDialectSelector()
-	 */
-	@Override
-	public DialectSelector getDialectSelector() {
-		return dialectSelector;
-	}
+  /** The url def. */
+  private UrlDef urlDef;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getName()
-	 */
-	@Override
-	public String getName() {
-		return name;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getDescription()
+   */
+  @Override
+  public String getDescription() {
+    return description;
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#setDialectSelector(net.witerat.cafenatedsql.spi.driver.DialectSelector)
-	 */
-	@Override
-	public void setDialectSelector(DialectSelector dialectSelector) {
-		this.dialectSelector = dialectSelector;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.ConnectionType#getDialectSelector()
+   */
+  @Override
+  public DialectSelector getDialectSelector() {
+    return dialectSelector;
+  }
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#setUrlDef(net.witerat.cafenatedsql.api.driver.UrlDef)
-	 */
-	@Override
-	public void setUrlDef(UrlDef url) {
-		urlDef=url;
-		
-	}
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getUrlDef()
-	 */
-	@Override
-	public UrlDef getUrlDef() {
-		return urlDef;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getName()
+   */
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.ConnectionType#setDialectSelector(net.
+   * witerat.cafenatedsql.spi.driver.DialectSelector)
+   */
+  @Override
+  public void setDialectSelector(final DialectSelector dialectSelector0) {
+    this.dialectSelector = dialectSelector0;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.ConnectionType#setUrlDef(net.witerat.
+   * cafenatedsql.api.driver.UrlDef)
+   */
+  @Override
+  public void setUrlDef(final UrlDef url) {
+    urlDef = url;
+
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.ConnectionType#getUrlDef()
+   */
+  @Override
+  public UrlDef getUrlDef() {
+    return urlDef;
+  }
 }
