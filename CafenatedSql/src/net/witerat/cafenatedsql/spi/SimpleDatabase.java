bae8a52133f26d5c14231f4f55267303d936e0b7
John Hutcheson <witerat.test@gmail.com>
2018-03-30 21:37:37 +1300

    Introduce Schema refactoring encapsulation.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/SimpleDatabase.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/SimpleDatabase.java
@@ -3,7 +3,6 @@ package net.witerat.cafenatedsql.spi;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Properties;
 
 import net.witerat.cafenatedsql.api.ConnectionFactory;
 import net.witerat.cafenatedsql.api.Database;
@@ -16,82 +15,255 @@ import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 import net.witerat.cafenatedsql.spi.driver.Driver;
 
 /**
- * Base inplementaion of database system to be refactored..
+ * Base implementation of database system to be refactored.
+ *
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
  *
  */
-public class SimpleDatabase implements Database {
+public class SimpleDatabase implements Database, SchemaManager {
 
-	private ConnectionFactory connectionFactory;
-	private Connection connection;
-	private Driver driver;
-	private Provider provider;
-	private List<Refactor> refactors;
+  /**
+   * The connectionFactory property.
+   */
+  private ConnectionFactory connectionFactory;
+  /**
+   * The connection property.
+   */
+  private Connection connection;
+  /**
+   * The driver property.
+   */
+  private Driver driver;
+  /**
+   * The provider property.
+   */
+  private Provider provider;
+  /**
+   * The refactors property.
+   */
+  private List<Refactor> refactors;
+  /**
+   * The schemas property.
+   */
+  private SchemaManager schemas = new SchemaManager() {
 
-	private SimpleDatabase() {
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    public Schema getSchema(final String name) {
+      Schema schema = provider.getSchemaFactory().getSchema(name);
+      schema.setJournal(refactors);
+      return schema;
+    }
 
-	/**
-	 * Instantiate a(n) SimpleDatabase object.
-	 * @param providerName
-	 * @param method
-	 * @param properties
-	 * @throws DriverCreationException
-	 */
-	public SimpleDatabase(String providerName, String method, TemplateEngineModel properties)
-			throws DriverCreationException {
-		ProviderRegistrar root = ProviderRegistrar.ROOT_REGISTRAR;
-		this.provider = root.getProvider(providerName);
-		TemplateEngineModel tem = provider.getModelFactory().newInstance(properties);
-		driver = this.provider.getDriverFactory().newDriver(tem);
-		connectionFactory = provider.getConnectionFactory(method);
-	}
+    @Override
+    public Schema getSchema() {
+      Schema newSchema = provider.getSchemaFactory().createSchema();
+      newSchema.setJournal(refactors);
+      return newSchema;
+    }
 
-	public void add(Schema schema) {
-		refactors.add((Refactor)schema);
-	}
+    @Override
+    public void add(final Schema schema) {
+      refactor(NOUN, Calculus.CREATE, schema);
 
-	/**
-	 * @return an anonymous Schema. 
-	 */
-	public Schema getSchema() {
-		Schema newSchema = provider.getSchemaFactory().createSchema();
-		newSchema.setJournal(refactors);
-		return newSchema;
+    }
 
-	}
+    @Override
+    public void drop(final Schema schema) {
+      refactor(NOUN, Calculus.DROP, schema);
 
-	/**
-	 * @param name The name of an existing schema.
-	 * @return A representation of the specified schema entity.
-	 */
-	public Schema getSchema(String name) {
-		Schema schema = provider.getSchemaFactory().getSchema(name);
-		schema.setJournal(refactors);
-		return schema;
+    }
 
-	}
+    /**
+     * @param noun
+     * @param operation
+     * @param obj
+     */
+    @Override
+    public void refactor(final String noun, final Calculus operation,
+        final Object obj) {
+      // TODO Auto-generated method stub
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Database#connect()
-	 */
-	@Override
-	public void connect() {
-		connection = connectionFactory.connect();
-	}
+    }
+  };
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Database#close()
-	 */
-	@Override
-	public void close() throws SQLException {
-		connection.close();
-		connection = null;
+  /**
+   * Instantiate a(n) SimpleDatabase object.
+   */
+  private SimpleDatabase() {
+    // TODO Auto-generated constructor stub
+  }
 
-	}
+  /**
+   * Instantiate a(n) SimpleDatabase object.
+   *
+   * @param providerName
+   *          name of a registered provider
+   * @param method
+   *          name of the connection method.
+   * @param properties
+   *          model and connection properties
+   * @throws DriverCreationException
+   *           if provider fails to provide a driver.
+   */
+  public SimpleDatabase(final String providerName, final String method,
+      final TemplateEngineModel properties)
+      throws DriverCreationException {
+    this();
+    ProviderRegistrar root = ProviderRegistrar.ROOT_REGISTRAR;
+    this.provider = root.getProvider(providerName);
+    init0(provider, provider.getConnectionFactory(method), properties);
+  }
+
+  /**
+   * Instantiate a(n) SimpleDatabase object.
+   *
+   * @param provider0
+   *          name of a register provider
+   * @param method
+   *          name of the connection method
+   * @param properties
+   *          model and connection properties.
+   * @throws DriverCreationException
+   *           if provider fails to provide a driver.
+   */
+
+  public SimpleDatabase(final Provider provider0, final String method,
+      final TemplateEngineModel properties)
+      throws DriverCreationException {
+
+    this.provider = provider0;
+    init0(provider0, provider0.getConnectionFactory(method), properties);
+  }
+
+  /**
+   * Instantiate a(n) SimpleDatabase object.
+   *
+   * @param p
+   *          A DBMS provider
+   * @param cf
+   *          a connection factory
+   * @param model
+   *          model and connection properties
+   * @throws DriverCreationException
+   *           if provider fails to provide a driver.
+   */
+
+  public SimpleDatabase(final Provider p, final ConnectionFactory cf,
+      final TemplateEngineModel model)
+      throws DriverCreationException {
+    init0(p, cf, model);
+  }
+
+  /**
+   * Common initialisation.
+   *
+   * @param provider0
+   *          a provider
+   * @param properties
+   *          the model and connection properties
+   * @param connectionFactory0
+   *          the connection factory.
+   * @throws DriverCreationException
+   *           if provider fails to provide a driver.
+   */
+
+  private void init0(final Provider provider0,
+      final ConnectionFactory connectionFactory0,
+      final TemplateEngineModel properties) throws DriverCreationException {
+    TemplateEngineModel tem =
+        provider0.getModelFactory().newInstance(properties);
+    driver = this.provider.getDriverFactory().newDriver(tem);
+    this.connectionFactory = connectionFactory0;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.spi.SchemaManager#add(net.witerat.cafenatedsql.
+   * api.Schema)
+   */
+  @Override
+  public void add(final Schema schema) {
+    schemas.add(schema);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.spi.SchemaManager#getSchema()
+   */
+  @Override
+  public Schema getSchema() {
+    return schemas.getSchema();
+
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.spi.SchemaManager#getSchema(java.lang.String)
+   */
+  @Override
+  public Schema getSchema(final String name) {
+    return schemas.getSchema(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void drop(final Schema schema) throws SQLException {
+    schemas.drop(schema);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Database#connect()
+   */
+  @Override
+  public void connect() {
+    connection = connectionFactory.connect();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Database#close()
+   */
+  @Override
+  public void close() throws SQLException {
+    connection.close();
+    connection = null;
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void update() throws SQLException {
+    for (Refactor refactor : refactors) {
+      submitRefactor(refactor);
+      connection.commit();
+    }
+
+  }
+
+  /**
+   * Ask the underlying dbms to implement the change.
+   * @param refactor0 then change to apply.
+   */
+  private void submitRefactor(final Refactor refactor0) {
+    refactor(refactor0.getNoun(), refactor0.getOperation(),
+        refactor0.getObject());
+
+  }
+  @Override
+  public void refactor(final String noun, final Calculus operation,
+      final Object obj) {
+    // TODO Auto-generated method stub
+
+  }
 }
