086ad72b39e829cf4e20cd702adaefecc1902bc8
John Hutcheson <witerat.test@gmail.com>
2018-03-30 21:59:39 +1300

    Revert "Add operations and JSLint review"
    This reverts commit 9541a66371817a5b8ab6de9646dbc1002ad1b09d.

--- CafenatedSql/src/net/witerat/cafenatedsql/api/Refactor.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/Refactor.java
@@ -2,28 +2,7 @@ package net.witerat.cafenatedsql.api;
 
 import java.util.List;
 
-import net.witerat.cafenatedsql.api.EntityManager.Calculus;
-
-/**
- * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
- *
- */
 public interface Refactor {
-  /**
-   * @param refactors a list of refactorings.
-   */
-  void setJournal(List<Refactor> refactors);
-  /**
-   * @return Dictionary noun for the verb.
-   */
-  String getNoun();
-  /**
-   * @return an operation to be performed
-   */
-  Calculus getOperation();
-  /**
-   * @return a reference to the modified object
-   */
-  Object getObject();
+	public abstract void setJournal(List<Refactor> refactors);
 
 }
