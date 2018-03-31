10e66b7270abd746f6f71af3a87147e1d7a6bacb
John Hutcheson <witerat.test@gmail.com>
2018-03-31 16:57:56 +1300

    JSLint

--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/SimpleMapModel.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/SimpleMapModel.java
@@ -3,65 +3,84 @@ package net.witerat.cafenatedsql.api.driver.template;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class SimpleMapModel.
+ * The Class SimpleMapModel implements a model using a map..
  */
 public class SimpleMapModel implements TemplateEngineModel {
-	
-	/** The properties. */
-	final Map<String, Object> properties;
-	
-	/**
-	 * Instantiates a new simple map model.
-	 *
-	 * @param p the p
-	 */
-	SimpleMapModel(Map<String, Object> p){
-		properties=p==null?new LinkedHashMap<>():p;
-	}
-	
-	/**
-	 * Instantiates a new simple map model.
-	 */
-	public SimpleMapModel() {
-		properties=new LinkedHashMap<>();
-	}
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#getByExpression(java.lang.String)
-	 */
-	@Override
-	public Object getByExpression(String name) {
-		try {
-			return evaluateContextExpression(name);
-		} catch (ExpressionFailedException e) {
-			return null;
-		}
-	}
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#get(java.lang.String)
-	 */
-	@Override
-	public Object get(String key) {
-		return properties.get(key);
-	}
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#set(java.lang.String, java.lang.Object)
-	 */
-	@Override
-	public void set(String property, Object value) {
-		properties.put(property, (String) value);
-	}
+  /** The properties. */
+  private final Map<String, Object> properties;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#evaluateContextExpression(java.lang.String)
-	 */
-	@Override
-	public Object evaluateContextExpression(String expression) throws ExpressionFailedException {
-		return get(expression);
-	}
+  /**
+   * Instantiates a new simple map model.
+   *
+   * @param p0
+   *          the p
+   */
+  SimpleMapModel(final Map<String, Object> p0) {
+    Map<String, Object> p = p0;
+    if (p == null) {
+      p = new LinkedHashMap<>();
+    }
+    properties = p;
+  }
+
+  /**
+   * Instantiates a new simple map model.
+   */
+  public SimpleMapModel() {
+    properties = new LinkedHashMap<>();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#
+   * getByExpression(java.lang.String)
+   */
+  @Override
+  public Object getByExpression(final String name) {
+    try {
+      return evaluateContextExpression(name);
+    } catch (ExpressionFailedException e) {
+      return null;
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#get(java.
+   * lang.String)
+   */
+  @Override
+  public Object get(final String key) {
+    return properties.get(key);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#set(java.
+   * lang.String, java.lang.Object)
+   */
+  @Override
+  public void set(final String property, final Object value) {
+    properties.put(property, (String) value);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#
+   * evaluateContextExpression(java.lang.String)
+   */
+  @Override
+  public Object evaluateContextExpression(final String expression)
+      throws ExpressionFailedException {
+    return get(expression);
+  }
 
 }
