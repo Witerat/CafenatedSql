39a53a4f9afdbf5b0abe0235ab0299be80b28dd8
John Hutcheson <witerat.test@gmail.com>
2018-03-31 17:24:52 +1300

    JSLint

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/TextContent.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/TextContent.java
@@ -3,39 +3,41 @@ package net.witerat.cafenatedsql.spi.driver.tags;
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 import net.witerat.cafenatedsql.spi.driver.Content;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class TextContent encapsulates static text content.
  */
-public class TextContent implements Content{
-	
-	/** The text. */
-	String text;
-	
-	/**
-	 * Instantiates a new empty text content.
-	 */
-	public TextContent() {
-		text="";
-	}
-	
-	/**
-	 * Instantiates a new text content.
-	 *
-	 * @param text the text
-	 */
-	public TextContent(String text){
-		this.text=text;
-	}
-	/**
-	 * Gets the text.
-	 *
-	 * @param model the model
-	 * @return the text
-	 */
-	@Override
-	public String getText(TemplateEngineModel model) {
-		return text;
-	}
+public class TextContent implements Content {
+
+  /** The text. */
+  private String text;
+
+  /**
+   * Instantiates a new empty text content.
+   */
+  public TextContent() {
+    text = "";
+  }
+
+  /**
+   * Instantiates a new text content.
+   *
+   * @param text0
+   *          the text
+   */
+  public TextContent(final String text0) {
+    this.text = text0;
+  }
+
+  /**
+   * Gets the text.
+   *
+   * @param model
+   *          the model
+   * @return the text
+   */
+  @Override
+  public String getText(final TemplateEngineModel model) {
+    return text;
+  }
 
 }
