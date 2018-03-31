9ff559d40c57ecfea82e040fbdfac5b986cce4d0
John Hutcheson <witerat.test@gmail.com>
2018-03-31 17:33:28 +1300

    JSLint

--- CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/SimplePropertiesModel.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/driver/template/SimplePropertiesModel.java
@@ -2,77 +2,94 @@ package net.witerat.cafenatedsql.api.driver.template;
 
 import java.util.Properties;
 
-// TODO: Auto-generated Javadoc
 /**
- * A simple implementation that backs a template model with a <code>java.util.Properties</code>
- * it has the limitation that values can only be strings.
+ * A simple implementation that backs a template model with a
+ * <code>java.util.Properties</code> it has the limitation that values can only
+ * be strings.
+ *
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
  *
  */
 public class SimplePropertiesModel implements TemplateEngineModel {
-	
-	/** The properties. */
-	private final Properties properties;
-	
-	/**
-	 * Instantiates a new simple properties model.
-	 *
-	 * @param p the p
-	 */
-	public SimplePropertiesModel(Properties p){
-		properties=p;
-	}
-	
-	/**
-	 * Instantiates a new simple properties model.
-	 */
-	public SimplePropertiesModel() {
-		properties=new Properties();
-	}
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#getByExpression(java.lang.String)
-	 */
-	@Override
-	public Object getByExpression(String name) {
-		try {
-			return evaluateContextExpression(name);
-		} catch (ExpressionFailedException e) {
-			return null;
-		}
-	}
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#get(java.lang.String)
-	 */
-	@Override
-	public Object get(String key) {
-		return getProperties().getProperty(key);
-	}
-	
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#set(java.lang.String, java.lang.Object)
-	 */
-	@Override
-	public void set(String property, Object value) {
-		 getProperties().setProperty(property, (String) value);
-	}
+  /** The properties. */
+  private final Properties properties;
 
-	/* (non-Javadoc)
-	 * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#evaluateContextExpression(java.lang.String)
-	 */
-	@Override
-	public Object evaluateContextExpression(String expression) throws ExpressionFailedException {
-		return get(expression);
-	}
+  /**
+   * Instantiates a new simple properties model.
+   *
+   * @param p
+   *          the p
+   */
+  public SimplePropertiesModel(final Properties p) {
+    properties = p;
+  }
 
-	/**
-	 * Gets the properties.
-	 *
-	 * @return the properties
-	 */
-	public Properties getProperties() {
-		return properties;
-	}
+  /**
+   * Instantiates a new simple properties model.
+   */
+  public SimplePropertiesModel() {
+    properties = new Properties();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#
+   * getByExpression(java.lang.String)
+   */
+  @Override
+  public Object getByExpression(final String name) {
+    try {
+      return evaluateContextExpression(name);
+    } catch (ExpressionFailedException e) {
+      return null;
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#get(java.
+   * lang.String)
+   */
+  @Override
+  public Object get(final String key) {
+    return getProperties().getProperty(key);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#set(java.
+   * lang.String, java.lang.Object)
+   */
+  @Override
+  public void set(final String property, final Object value) {
+    getProperties().setProperty(property, (String) value);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel#
+   * evaluateContextExpression(java.lang.String)
+   */
+  @Override
+  public Object evaluateContextExpression(final String expression)
+      throws ExpressionFailedException {
+    return get(expression);
+  }
+
+  /**
+   * Gets the properties.
+   *
+   * @return the properties
+   */
+  public Properties getProperties() {
+    return properties;
+  }
 
 }
