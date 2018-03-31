bfc5b1af9b69af2b825b7fb558fd1b095dd2e69d
John Hutcheson <witerat.test@gmail.com>
2018-03-31 17:26:33 +1300

    JSLint

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/SqlTemplate.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/SqlTemplate.java
@@ -10,43 +10,53 @@ import javax.xml.bind.annotation.XmlType;
 /**
  * The class SqlTemplate.
  */
-@XmlType(name="TemplateTag")
+@XmlType(name = "TemplateTag")
 public class SqlTemplate {
-	
-	/** The parameters. */
-	Map<String, ParameterTag> parametersByName;
 
-	/**
-	 * Gets the parameters.
-	 *
-	 * @return the parameters
-	 */
-	@XmlTransient
-	public Collection<ParameterTag> getParameters() {
-		if(parametersByName==null)parametersByName=new LinkedHashMap<>();
-		
-		return parametersByName.values();
-	}
+  /** The parameters. */
+  private Map<String, ParameterTag> parametersByName;
 
-	/**
-	 * Sets the parameters.
-	 *
-	 * @param parameters the new parameters
-	 */
-	public void setParameters(Collection<ParameterTag> parameters) {
-		if(parameters==null) parametersByName=null;
-		else {
-			Map<String, ParameterTag> pBN=new LinkedHashMap<>();
-			for(ParameterTag p:parameters) pBN.put(p.getName(), p);
-			parametersByName = pBN;
-		}
-	}
+  /**
+   * Gets the parameters.
+   *
+   * @return the parameters
+   */
+  @XmlTransient
+  public Collection<ParameterTag> getParameters() {
+    if (parametersByName == null) {
+      parametersByName = new LinkedHashMap<>();
+    }
 
-	/**
-	 * @param param a new parameter
-	 */
-	public void setParameter(ParameterTag param) {
-		if(parametersByName==null)parametersByName=new LinkedHashMap<>();
-		parametersByName.put(param.getName(), param);
-	}
+    return parametersByName.values();
+  }
+
+  /**
+   * Sets the parameters.
+   *
+   * @param parameters
+   *          the new parameters
+   */
+  public void setParameters(final Collection<ParameterTag> parameters) {
+    if (parameters == null) {
+      parametersByName = null;
+    } else {
+      Map<String, ParameterTag> parsByName = new LinkedHashMap<>();
+      for (ParameterTag p : parameters) {
+        parsByName.put(p.getName(), p);
+      }
+      parametersByName = parsByName;
+    }
+  }
+
+  /**
+   * Add parameters.
+   * @param param
+   *          a new parameter
+   */
+  public void setParameter(final ParameterTag param) {
+    if (parametersByName == null) {
+      parametersByName = new LinkedHashMap<>();
+    }
+    parametersByName.put(param.getName(), param);
+  }
 }
