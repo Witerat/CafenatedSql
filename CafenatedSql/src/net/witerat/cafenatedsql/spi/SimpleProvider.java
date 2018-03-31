e6cd6a82ff5eb8dc5813785d77fa4c33bbb4e8a2
John Hutcheson <witerat.test@gmail.com>
2018-03-31 17:32:39 +1300

    JSLint & Javadoc

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/SimpleProvider.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/SimpleProvider.java
@@ -16,362 +16,391 @@ import net.witerat.cafenatedsql.api.URLFactory;
 import net.witerat.cafenatedsql.api.VerbFactory;
 import net.witerat.cafenatedsql.api.ViewFactory;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class SimpleProvider. This provider can be configured use a single object
  * to implement any or all of the factory services offers.
  */
 public class SimpleProvider implements Provider {
 
-	/** The connection factory. */
-	private ConnectionFactory connectionFactory;
+  /** The connection factory. */
+  private ConnectionFactory connectionFactory;
 
-	/** The dialect factory. */
-	private DialectFactory dialectFactory;
+  /** The dialect factory. */
+  private DialectFactory dialectFactory;
 
-	/** The driver class. */
-	private String driverClass;
+  /** The driver class. */
+  private String driverClass;
 
-	/** The driver factory. */
-	private DriverFactory driverFactory;
+  /** The driver factory. */
+  private DriverFactory driverFactory;
 
-	/** The grammar factory. */
-	private GrammarFactory grammarFactory;
+  /** The grammar factory. */
+  private GrammarFactory grammarFactory;
 
-	/** The noun factory. */
-	private NounFactory nounFactory;
+  /** The noun factory. */
+  private NounFactory nounFactory;
 
-	/** The parameter factory. */
-	private ParameterFactory parameterFactory;
+  /** The parameter factory. */
+  private ParameterFactory parameterFactory;
 
-	/** The schema factory. */
-	private SchemaFactory schemaFactory;
+  /** The schema factory. */
+  private SchemaFactory schemaFactory;
 
-	/** The table factory. */
-	private TableFactory tableFactory;
+  /** The table factory. */
+  private TableFactory tableFactory;
 
-	/** The URL factory. */
-	private URLFactory urlFactory;
+  /** The URL factory. */
+  private URLFactory urlFactory;
 
-	/** The verb factory. */
-	private VerbFactory verbFactory;
+  /** The verb factory. */
+  private VerbFactory verbFactory;
 
-	/** The view factory. */
-	private ViewFactory viewFactory;
+  /** The view factory. */
+  private ViewFactory viewFactory;
 
-	private DatabaseFactory databaseFactory;
+  /** The databaseFactory property. */
+  private DatabaseFactory databaseFactory;
 
-	private TemplateModelFactory modelFactory;
+  /** The modelFactory property. */
+  private TemplateModelFactory modelFactory;
 
-	/**
-	 * Configures this provider to expose the factory services implemented by a
-	 * specified factory.
-	 * 
-	 * @param factory
-	 *            an implementor of factory some of the services published by
-	 *            the provider.
-	 */
-	public void configure(CafenatedFactory factory) {
-		if (factory instanceof ConnectionFactory)
-			connectionFactory = (ConnectionFactory) factory;
-		if (factory instanceof SchemaFactory)
-			schemaFactory = (SchemaFactory) factory;
-		if (factory instanceof TableFactory)
-			tableFactory = (TableFactory) factory;
-		if (factory instanceof ViewFactory)
-			verbFactory = (VerbFactory) factory;
-		if (factory instanceof DatabaseFactory)
-			databaseFactory = (DatabaseFactory) factory;
-		if (factory instanceof DriverFactory)
-			driverFactory = (DriverFactory) factory;
-		if (factory instanceof GrammarFactory)
-			grammarFactory = (GrammarFactory) factory;
-		if (factory instanceof DialectFactory)
-			dialectFactory = (DialectFactory) factory;
-		if (factory instanceof TemplateModelFactory)
-			modelFactory=(TemplateModelFactory) factory;
-		if (factory instanceof NounFactory)
-			nounFactory = (NounFactory) factory;
-		if (factory instanceof VerbFactory)
-			verbFactory = (VerbFactory) factory;
-		if (factory instanceof ParameterFactory)
-			parameterFactory = (ParameterFactory) factory;
-		if (factory instanceof URLFactory)
-			urlFactory = (URLFactory) factory;
-	}
+  /**
+   * Configures this provider to expose the factory services implemented by a
+   * specified factory.
+   *
+   * @param factory0
+   *          an implementor of factory some of the services published by the
+   *          provider.
+   */
+  public void configure(final CafenatedFactory factory0) {
+    if (factory0 instanceof ConnectionFactory) {
+      connectionFactory = (ConnectionFactory) factory0;
+    }
+    if (factory0 instanceof SchemaFactory) {
+      schemaFactory = (SchemaFactory) factory0;
+    }
+    if (factory0 instanceof TableFactory) {
+      tableFactory = (TableFactory) factory0;
+    }
+    if (factory0 instanceof ViewFactory) {
+      verbFactory = (VerbFactory) factory0;
+    }
+    if (factory0 instanceof DatabaseFactory) {
+      databaseFactory = (DatabaseFactory) factory0;
+    }
+    if (factory0 instanceof DriverFactory) {
+      driverFactory = (DriverFactory) factory0;
+    }
+    if (factory0 instanceof GrammarFactory) {
+      grammarFactory = (GrammarFactory) factory0;
+    }
+    if (factory0 instanceof DialectFactory) {
+      dialectFactory = (DialectFactory) factory0;
+    }
+    if (factory0 instanceof TemplateModelFactory) {
+      modelFactory = (TemplateModelFactory) factory0;
+    }
+    if (factory0 instanceof NounFactory) {
+      nounFactory = (NounFactory) factory0;
+    }
+    if (factory0 instanceof VerbFactory) {
+      verbFactory = (VerbFactory) factory0;
+    }
+    if (factory0 instanceof ParameterFactory) {
+      parameterFactory = (ParameterFactory) factory0;
+    }
+    if (factory0 instanceof URLFactory) {
+      urlFactory = (URLFactory) factory0;
+    }
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getConnectionFactory()
-	 */
-	@Override
-	public ConnectionFactory getConnectionFactory() {
-		return connectionFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getConnectionFactory()
+   */
+  @Override
+  public ConnectionFactory getConnectionFactory() {
+    return connectionFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getDialectFactory()
-	 */
-	@Override
-	public DialectFactory getDialectFactory() {
+  /**
+   * {@inheritDoc}
+   * @see net.witerat.cafenatedsql.api.Provider#getConnectionFactory(
+   *    java.lang.String)
+   */
+  @Override
+  public ConnectionFactory getConnectionFactory(final String method0) {
+    // TODO Auto-generated method stub
+    return connectionFactory;
+  }
 
-		return dialectFactory;
-	}
+  /* (non-Javadoc)
+   * @see net.witerat.cafenatedsql.api.Provider#getConnectionMethods()
+   */
+  @Override
+  public String[] getConnectionMethods() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getDriverClass()
-	 */
-	@Override
-	public String getDriverClass() {
-		return driverClass;
-	}
+  /* (non-Javadoc)
+   * @see net.witerat.cafenatedsql.api.Provider#getDatabaseFactory()
+   */
+  @Override
+  public DatabaseFactory getDatabaseFactory() {
+    // TODO Auto-generated method stub
+    return databaseFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getDriverFactory()
-	 */
-	@Override
-	public DriverFactory getDriverFactory() {
-		return driverFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getDialectFactory()
+   */
+  @Override
+  public DialectFactory getDialectFactory() {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getGrammarFactory()
-	 */
-	@Override
-	public GrammarFactory getGrammarFactory() {
-		return grammarFactory;
-	}
+    return dialectFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getNounFactory()
-	 */
-	@Override
-	public NounFactory getNounFactory() {
-		return nounFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getDriverClass()
+   */
+  @Override
+  public String getDriverClass() {
+    return driverClass;
+  }
 
-	/**
-	 * Gets the paramater factory.
-	 *
-	 * @return the paramater factory
-	 */
-	public ParameterFactory getParamaterFactory() {
-		return parameterFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getDriverFactory()
+   */
+  @Override
+  public DriverFactory getDriverFactory() {
+    return driverFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getParameterFactory()
-	 */
-	@Override
-	public ParameterFactory getParameterFactory() {
-		return parameterFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getGrammarFactory()
+   */
+  @Override
+  public GrammarFactory getGrammarFactory() {
+    return grammarFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getSchemaFactory()
-	 */
-	@Override
-	public SchemaFactory getSchemaFactory() {
-		return schemaFactory;
-	}
+  /**
+   * {@inheritDoc}
+   * @see net.witerat.cafenatedsql.api.Provider#getModelFactory()
+   */
+  @Override
+  public TemplateModelFactory getModelFactory() {
+    // TODO Auto-generated method stub
+    return modelFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getTableFactory()
-	 */
-	@Override
-	public TableFactory getTableFactory() {
-		return tableFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getNounFactory()
+   */
+  @Override
+  public NounFactory getNounFactory() {
+    return nounFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getUrlFactory()
-	 */
-	@Override
-	public URLFactory getUrlFactory() {
-		return urlFactory;
-	}
+  /**
+   * Gets the parameter factory.
+   *
+   * @return the parameter factory
+   */
+  public ParameterFactory getParamaterFactory() {
+    return parameterFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getVerbFactory()
-	 */
-	@Override
-	public VerbFactory getVerbFactory() {
-		return verbFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getParameterFactory()
+   */
+  @Override
+  public ParameterFactory getParameterFactory() {
+    return parameterFactory;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.witerat.cafenatedsql.api.Provider#getViewFactory()
-	 */
-	@Override
-	public ViewFactory getViewFactory() {
-		return viewFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getSchemaFactory()
+   */
+  @Override
+  public SchemaFactory getSchemaFactory() {
+    return schemaFactory;
+  }
 
-	/**
-	 * Sets the connection factory.
-	 *
-	 * @param connectionFactory
-	 *            the new connection factory
-	 */
-	public void setConnectionFactory(ConnectionFactory connectionFactory) {
-		this.connectionFactory = connectionFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getTableFactory()
+   */
+  @Override
+  public TableFactory getTableFactory() {
+    return tableFactory;
+  }
 
-	/**
-	 * Sets the dialect factory.
-	 *
-	 * @param dialectFactory
-	 *            the new dialect factory
-	 */
-	public void setDialectFactory(DialectFactory dialectFactory) {
-		this.dialectFactory = dialectFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getUrlFactory()
+   */
+  @Override
+  public URLFactory getUrlFactory() {
+    return urlFactory;
+  }
 
-	/**
-	 * Sets the driver class.
-	 *
-	 * @param driverClass
-	 *            the new driver class
-	 */
-	public void setDriverClass(String driverClass) {
-		this.driverClass = driverClass;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getVerbFactory()
+   */
+  @Override
+  public VerbFactory getVerbFactory() {
+    return verbFactory;
+  }
 
-	/**
-	 * Sets the driver factory.
-	 *
-	 * @param driverFactory
-	 *            the new driver factory
-	 */
-	public void setDriverFactory(DriverFactory driverFactory) {
-		this.driverFactory = driverFactory;
-	}
+  /*
+   * (non-Javadoc)
+   *
+   * @see net.witerat.cafenatedsql.api.Provider#getViewFactory()
+   */
+  @Override
+  public ViewFactory getViewFactory() {
+    return viewFactory;
+  }
 
-	/**
-	 * Sets the grammar factory.
-	 *
-	 * @param grammarFactory
-	 *            the new grammar factory
-	 */
-	public void setGrammarFactory(GrammarFactory grammarFactory) {
-		this.grammarFactory = grammarFactory;
-	}
+  /**
+   * Sets the connection factory.
+   *
+   * @param connectionFactory0
+   *          the new connection factory
+   */
+  public void setConnectionFactory(final ConnectionFactory connectionFactory0) {
+    this.connectionFactory = connectionFactory0;
+  }
 
-	/**
-	 * Sets the noun factory.
-	 *
-	 * @param nounFactory
-	 *            the new noun factory
-	 */
-	public void setNounFactory(NounFactory nounFactory) {
-		this.nounFactory = nounFactory;
-	}
+  /**
+   * Sets the dialect factory.
+   *
+   * @param dialectFactory0
+   *          the new dialect factory
+   */
+  public void setDialectFactory(final DialectFactory dialectFactory0) {
+    this.dialectFactory = dialectFactory0;
+  }
 
-	/**
-	 * Sets the parameter factory.
-	 *
-	 * @param paramaterFactory
-	 *            the new parameter factory
-	 */
-	public void setParamaterFactory(ParameterFactory paramaterFactory) {
-		this.parameterFactory = paramaterFactory;
-	}
+  /**
+   * Sets the driver class.
+   *
+   * @param driverClass0
+   *          the new driver class
+   */
+  public void setDriverClass(final String driverClass0) {
+    this.driverClass = driverClass0;
+  }
 
-	/**
-	 * Sets the schema factory.
-	 *
-	 * @param schemaFactory
-	 *            the new schema factory
-	 */
-	public void setSchemaFactory(SchemaFactory schemaFactory) {
-		this.schemaFactory = schemaFactory;
-	}
+  /**
+   * Sets the driver factory.
+   *
+   * @param driverFactory0
+   *          the new driver factory
+   */
+  public void setDriverFactory(final DriverFactory driverFactory0) {
+    this.driverFactory = driverFactory0;
+  }
 
-	/**
-	 * Sets the table factory.
-	 *
-	 * @param tableFactory
-	 *            the new table factory
-	 */
-	public void setTableFactory(TableFactory tableFactory) {
-		this.tableFactory = tableFactory;
-	}
+  /**
+   * Sets the grammar factory.
+   *
+   * @param grammarFactory0
+   *          the new grammar factory
+   */
+  public void setGrammarFactory(final GrammarFactory grammarFactory0) {
+    this.grammarFactory = grammarFactory0;
+  }
 
-	/**
-	 * Sets the url factory.
-	 *
-	 * @param urlFactory
-	 *            the new url factory
-	 */
-	public void setUrlFactory(URLFactory urlFactory) {
-		this.urlFactory = urlFactory;
-	}
+  /**
+   * Sets the noun factory.
+   *
+   * @param nounFactory0
+   *          the new noun factory
+   */
+  public void setNounFactory(final NounFactory nounFactory0) {
+    this.nounFactory = nounFactory0;
+  }
 
-	/**
-	 * Sets the verb factory.
-	 *
-	 * @param verbFactory
-	 *            the new verb factory
-	 */
-	public void setVerbFactory(VerbFactory verbFactory) {
-		this.verbFactory = verbFactory;
-	}
+  /**
+   * Sets the parameter factory.
+   *
+   * @param paramaterFactory0
+   *          the new parameter factory
+   */
+  public void setParamaterFactory(final ParameterFactory paramaterFactory0) {
+    this.parameterFactory = paramaterFactory0;
+  }
 
-	/**
-	 * Sets the view factory.
-	 *
-	 * @param viewFactory
-	 *            the new view factory
-	 */
-	public void setViewFactory(ViewFactory viewFactory) {
-		this.viewFactory = viewFactory;
-	}
+  /**
+   * Sets the schema factory.
+   *
+   * @param schemaFactory0
+   *          the new schema factory
+   */
+  public void setSchemaFactory(final SchemaFactory schemaFactory0) {
+    this.schemaFactory = schemaFactory0;
+  }
 
-	@Override
-	public String[] getConnectionMethods() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  /**
+   * Sets the table factory.
+   *
+   * @param tableFactory0
+   *          the new table factory
+   */
+  public void setTableFactory(final TableFactory tableFactory0) {
+    this.tableFactory = tableFactory0;
+  }
 
-	@Override
-	public ConnectionFactory getConnectionFactory(String method) {
-		// TODO Auto-generated method stub
-		return connectionFactory;
-	}
+  /**
+   * Sets the url factory.
+   *
+   * @param urlFactory0
+   *          the new url factory
+   */
+  public void setUrlFactory(final URLFactory urlFactory0) {
+    this.urlFactory = urlFactory0;
+  }
 
-	@Override
-	public DatabaseFactory getDatabaseFactory() {
-		// TODO Auto-generated method stub
-		return databaseFactory;
-	}
+  /**
+   * Sets the verb factory.
+   *
+   * @param verbFactory0
+   *          the new verb factory
+   */
+  public void setVerbFactory(final VerbFactory verbFactory0) {
+    this.verbFactory = verbFactory0;
+  }
 
-	@Override
-	public TemplateModelFactory getModelFactory() {
-		// TODO Auto-generated method stub
-		return modelFactory;
-	}
+  /**
+   * Sets the view factory.
+   *
+   * @param viewFactory0
+   *          the new view factory
+   */
+  public void setViewFactory(final ViewFactory viewFactory0) {
+    this.viewFactory = viewFactory0;
+  }
 
 }
