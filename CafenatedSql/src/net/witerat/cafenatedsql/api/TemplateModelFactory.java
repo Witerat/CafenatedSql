4f3dbccf29669d32cd4aee91e4e5152fd0aa2a9d
John Hutcheson <witerat.test@gmail.com>
2018-03-31 17:34:44 +1300

    JSLint & Javadoc

--- CafenatedSql/src/net/witerat/cafenatedsql/api/TemplateModelFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/TemplateModelFactory.java
@@ -4,10 +4,26 @@ import java.util.Properties;
 
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 
-public interface TemplateModelFactory extends CafenatedFactory{
+/**
+ * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
+ *
+ */
+public interface TemplateModelFactory extends CafenatedFactory {
 
-	TemplateEngineModel newInstance(Properties properties);
+  /**
+   * Creates a new model.
+   *
+   * @param defaults default properties of the model.
+   * @return a new model
+   */
+  TemplateEngineModel newInstance(Properties defaults);
 
-	TemplateEngineModel newInstance(TemplateEngineModel model);
+  /**
+   * Creates an extension of the specified model.
+   *
+   * @param model a model providing defaults.
+   * @return a new model which draws defaults values from <code>model</code>
+   */
+  TemplateEngineModel newInstance(TemplateEngineModel model);
 
 }
