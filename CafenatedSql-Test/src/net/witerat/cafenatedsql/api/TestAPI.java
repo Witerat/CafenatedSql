1b4a05b0348cad34cefbaadbf49b9a3501c1895a
John Hutcheson <witerat.test@gmail.com>
2018-04-02 12:52:22 +1200

    Checkstyle

--- CafenatedSql-Test/src/net/witerat/cafenatedsql/api/TestAPI.java
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/api/TestAPI.java
@@ -15,56 +15,57 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
 
-
 public class TestAPI {
-	@SuppressWarnings({ "unchecked", "serial", "rawtypes" })
-	Map<String, Object> mockConProp=new LinkedHashMap(){
-		{
-			put("connection_method", "mock");
-		}
-	};
+  @SuppressWarnings({ "unchecked", "serial", "rawtypes" })
+  Map<String, Object> mockConProp = new LinkedHashMap() {
+    {
+      put("connection_method", "mock");
+    }
+  };
 
-	@Test
-	public void testConnect(){
-		ProviderRegistrar reg=ProviderRegistrar.ROOT_REGISTRAR;
-		Provider prov=reg.getProvider("mock");
-		ConnectionFactory cf= prov.getConnectionFactory("mock");
-		Connection c=cf.connect();
-		assertNotNull(c);
-	}
+  @Test
+  public void testConnect() {
+    ProviderRegistrar reg = ProviderRegistrar.ROOT_REGISTRAR;
+    Provider prov = reg.getProvider("mock");
+    ConnectionFactory cf = prov.getConnectionFactory("mock");
+    Connection c = cf.connect();
+    assertNotNull(c);
+  }
 
-	public void testDatabase(){
-		ProviderRegistrar reg=ProviderRegistrar.ROOT_REGISTRAR;
-		Provider provider= reg.getProvider("mock");
-		ConnectionFactory cf=provider.getConnectionFactory("mock");
-		Properties properties=new Properties();
-		properties.putAll(mockConProp);
-		TemplateEngineModel model=provider.getModelFactory().newInstance(properties);
-		
-		Database db=null;
-		try {
-			db = provider.getDatabaseFactory().newDatabase(model);
-		} catch (DriverCreationException e) {
-			// TODO Auto-generated catch block
-			fail("Exception "+e.getLocalizedMessage());
-			throw new RuntimeException(e);
-		}
-		Schema sch=provider.getSchemaFactory().createSchema();
-		sch.setName("mock");
-		db.add(sch);
-		
-	}
-	/**
-	 * 
-	 */
-	@Before
-	public void beforeTest() {
-			ProviderRegistrar reg=ProviderRegistrar.ROOT_REGISTRAR;
-		MockProvider mockProv = new MockProvider();
-		ConnectionFactory mcf=new MockConnectionFactory(mockConProp);
-		mockProv.setConnectionFactory(mcf);
-		
-		reg.registerProvider(mockProv, "mock", "(c) 1967", "Buffoon DB", "test model db", "Stupidity Inc.");
-		
-	}
+  public void testDatabase() {
+    ProviderRegistrar reg = ProviderRegistrar.ROOT_REGISTRAR;
+    Provider provider = reg.getProvider("mock");
+    ConnectionFactory cf = provider.getConnectionFactory("mock");
+    Properties properties = new Properties();
+    properties.putAll(mockConProp);
+    TemplateEngineModel model = provider.getModelFactory().newInstance(properties);
+
+    Database db = null;
+    try {
+      db = provider.getDatabaseFactory().newDatabase(model);
+    } catch (DriverCreationException e) {
+      // TODO Auto-generated catch block
+      fail("Exception " + e.getLocalizedMessage());
+      throw new RuntimeException(e);
+    }
+    Schema sch = provider.getSchemaFactory().createSchema();
+    sch.setName("mock");
+    db.add(sch);
+
+  }
+
+  /**
+   * 
+   */
+  @Before
+  public void beforeTest() {
+    ProviderRegistrar reg = ProviderRegistrar.ROOT_REGISTRAR;
+    MockProvider mockProv = new MockProvider();
+    ConnectionFactory mcf = new MockConnectionFactory(mockConProp);
+    mockProv.setConnectionFactory(mcf);
+
+    reg.registerProvider((Provider) mockProv, "mock", "(c) 1967", "Buffoon DB", "test model db",
+        "Stupidity Inc.");
+
+  }
 }
