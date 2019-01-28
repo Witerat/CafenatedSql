f708b7f886b2b0fbcc74672317de4b1cdaf052b4
John Hutcheson <witerat.test@gmail.com>
2019-01-29 10:35:17 +1300

    Created <code>simple</code> package for SimpleExpressionLanguage encapsulation.

---
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/template/simple/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The standard package implementation for simple template expressions.
+ * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
+ */
+package net.witerat.cafenatedsql.spi.driver.template.simple;
+
