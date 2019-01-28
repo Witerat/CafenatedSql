10dcdce5d280f8dfcfef67a4681b00067fbcd37f
John Hutcheson <witerat.test@gmail.com>
2019-01-29 10:38:25 +1300

    Moved to <code>simple</code> sub-package because test target moved.

---
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/spi/driver/template/simple/SimpleExpressionLanguageTest.java
@@ -0,0 +1,41 @@
+package net.witerat.cafenatedsql.spi.driver.template.simple;
+
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.witerat.cafenatedsql.api.driver.template.ExpressionFailedException;
+import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
+import net.witerat.cafenatedsql.spi.SimpleModelFactory;
+import net.witerat.cafenatedsql.spi.driver.template.simple.SimpleExpressionLanguage;
+
+public class SimpleExpressionLanguageTest {
+
+  SimpleExpressionLanguage fixture;
+  @Before
+  public void setup(){
+    fixture= new SimpleExpressionLanguage();
+  }
+  @Test
+  public void testEvaluate() {
+    TemplateEngineModel model=new SimpleModelFactory().newInstance(new Properties(){
+      /** The serialVersionUID property. */
+      private static final long serialVersionUID = 1L;
+      {
+        put("x", 5);
+      }
+    });
+    Object r=null;
+    try {
+      r = fixture.evaluate("3", model);
+      assertNotNull(r);
+    } catch (ExpressionFailedException e) {
+      fail("Exception "+e.getClass().getName()+": "+e.getMessage());
+    }
+    assertEquals(Integer.valueOf(3),r);
+  }
+
+}
