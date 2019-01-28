3ad33825fede61a2d558b4571cbb880763418ca2
John Hutcheson <witerat.test@gmail.com>
2019-01-29 10:32:48 +1300

    Refactored to top level out of SimpleExpressionLanguage for simplicity.

---
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/template/simple/Ops.java
@@ -0,0 +1,58 @@
+package net.witerat.cafenatedsql.spi.driver.template.simple;
+
+/** Operators. */
+enum Ops {
+  /** open parenthesis. */
+  PARENO(17),
+  /** close parenthesis. */
+  PARENC(18),
+
+  /** Assignments. */
+  SET(16), SET_ADD(16), SET_SUB(16), SET_MUL(16), SET_DIV(16), SET_MOD(16),
+  /** Logical assignments. */
+  SET_AND(16), SET_XOR(16), SET_OR(16),
+  /** Shift assignments. */
+  SET_LSH(16), SET_RSH(16), SET_URS(16),
+
+  /** Type cast. */
+  CAST(15),
+  /** Ternary operator. */
+  TERN2(14), TERN1(13),
+  /** Logical operators. */
+  ORL(12), ANDL(11), ORB(10), XORB(9), ANDB(8),
+
+  /** Relational Operators. */
+  EQ(7), NE(7), LT(6), LE(6), GT(6), GE(6), INSTANCEOF(6),
+  /** Shift operators. */
+  LSHIFT(5), RSHIFT(5), URSHIFT(5),
+  /** Terms. */
+  ADDITION(4), SUBTRACTION(4),
+  /** Products. */
+  MULTIPLICATION(3), DIVISION(3), MODULUS(3),
+  /** Unary operators. */
+  UINCR(2), UDECR(2), UPLUS(2), UMINUS(2), NOTB(2), NOTL(2),
+  /** Counter operators.*/
+  INCR(1), DECR(1),
+
+  /** Values. */
+  LITERAL(0), PROPERTY(0);
+  /**
+   * Instantiate operator enumeration value object.
+   * @param priority
+   *          the precedence value.
+   */
+  Ops(final int priority) {
+    nPriority = priority;
+  }
+
+  /** the priority of the operation). */
+  private final int nPriority;
+
+  /**
+   * Get the precedence of the operation.
+   * @return this operation's precedence.
+   */
+  int precedence() {
+    return nPriority;
+  }
+}
