9bb51b670e74385a7e7e4655320399330a4852dc
John Hutcheson <witerat.test@gmail.com>
2019-02-02 19:36:09 +1300

    Moved to API, implemented  newDatabase and newDriver methods

--- CafenatedSql-Test/src/net/witerat/cafenatedsql/api/mock/MockDriverFactory.java
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/api/mock/MockDriverFactory.java
@@ -1,4 +1,4 @@
-package net.witerat.cafenatedsql.spi.mock;
+package net.witerat.cafenatedsql.api.mock;
 
 import net.witerat.cafenatedsql.api.Database;
 import net.witerat.cafenatedsql.api.DatabaseFactory;
@@ -14,73 +14,74 @@ import net.witerat.cafenatedsql.api.driver.VerbDef;
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 import net.witerat.cafenatedsql.spi.driver.DialectSelector;
 import net.witerat.cafenatedsql.spi.driver.Driver;
+import net.witerat.cafenatedsql.spi.mock.MockDDLDriver;
 
 public class MockDriverFactory implements DriverFactory, DatabaseFactory {
 
-	@Override
-	public Database newDatabase(TemplateEngineModel model) throws DriverCreationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Database newDatabase(TemplateEngineModel model) throws DriverCreationException {
+    return new MockDatabase(model);
+  }
 
-	@Override
-	public Driver newDriver(TemplateEngineModel model) throws DriverCreationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Driver newDriver(TemplateEngineModel model) throws DriverCreationException {
+    MockDDLDriver mockDDLDriver = new MockDDLDriver();
+    mockDDLDriver.setModel(model);
+    return mockDDLDriver;
+  }
 
-	@Override
-	public ConnectionType newConnectionType(Driver driver, String method) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public ConnectionType newConnectionType(Driver driver, String method) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public DialectSelector newDialectExpressionSelector(ConnectionType ct, String nameExpression) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public DialectSelector newDialectExpressionSelector(ConnectionType ct, String nameExpression) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public DialectSelector newDialectNameSelector(ConnectionType ct, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public DialectSelector newDialectNameSelector(ConnectionType ct, String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public GrammarDef newGrammar(DialectDef dd, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public GrammarDef newGrammar(DialectDef dd, String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public VerbDef newVerb(NounDef nn, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public VerbDef newVerb(NounDef nn, String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public NounDef newNoun(GrammarDef gg, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public NounDef newNoun(GrammarDef gg, String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public DialectDef newDialectDefinition(Driver driver, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public DialectDef newDialectDefinition(Driver driver, String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public UrlDef newUrlDef(ConnectionType ct) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public UrlDef newUrlDef(ConnectionType ct) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public ParameterType newParameter() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public ParameterType newParameter() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
