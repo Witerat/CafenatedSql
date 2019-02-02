d7b4c12f75087a17362cf360b23412d87a14b899
John Hutcheson <witerat.test@gmail.com>
2019-02-02 19:33:27 +1300

    Added factory methods.

--- CafenatedSql-Test/src/net/witerat/cafenatedsql/api/mock/MockModelFactory.java
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/api/mock/MockModelFactory.java
@@ -1,22 +1,80 @@
-package net.witerat.cafenatedsql.spi.mock;
+package net.witerat.cafenatedsql.api.mock;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import net.witerat.cafenatedsql.api.TemplateModelFactory;
+import net.witerat.cafenatedsql.api.driver.template.ExpressionFailedException;
 import net.witerat.cafenatedsql.api.driver.template.TemplateEngineModel;
 
 public class MockModelFactory implements TemplateModelFactory {
 
-	@Override
-	public TemplateEngineModel newInstance(Properties properties) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public TemplateEngineModel newInstance(final Properties properties) {
+    return new TemplateEngineModel() {
+      Map<String, Object> map = new LinkedHashMap<>();
 
-	@Override
-	public TemplateEngineModel newInstance(TemplateEngineModel model) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+      @Override
+      public Object get(String property) {
+        return get0(property);
+      }
+
+      private Object get0(String property) {
+        if (map.containsKey(property)) {
+          return map.get(property);
+        }
+        return properties.get(property);
+      }
+      @Override
+      public void set(String property, Object value) {
+        map.put(property, value);
+      }
+
+      @Override
+      public Object getByExpression(String expression) {
+        return get0(expression);
+      }
+
+      @Override
+      public Object evaluateContextExpression(String expression) throws ExpressionFailedException {
+        return get0(expression);
+      }
+    };
+  }
+
+  @Override
+  public TemplateEngineModel newInstance(TemplateEngineModel model) {
+    return new TemplateEngineModel() {
+      Map<String, Object> map = new LinkedHashMap<>();
+
+      @Override
+      public Object get(String property) {
+        return get0(property);
+      }
+
+      private Object get0(String property) {
+        if (map.containsKey(property)) {
+          return map.get(property);
+        }
+        return model.get(property);
+      }
+      @Override
+      public void set(String property, Object value) {
+        map.put(property, value);
+      }
+
+      @Override
+      public Object getByExpression(String expression) {
+        return get0(expression);
+      }
+
+      @Override
+      public Object evaluateContextExpression(String expression) throws ExpressionFailedException {
+        // TODO Auto-generated method stub
+        return get0(expression);
+      }
+    };
+  }
 
 }
