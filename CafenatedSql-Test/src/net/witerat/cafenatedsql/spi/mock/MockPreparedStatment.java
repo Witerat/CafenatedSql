376850287eeebde9c6759e79024ed903717566c3
John Hutcheson <witerat.test@gmail.com>
2019-02-02 19:31:22 +1300

    Change AG-TODOs in no-ops, added logging

--- CafenatedSql-Test/src/net/witerat/cafenatedsql/spi/mock/MockPreparedStatment.java
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/spi/mock/MockPreparedStatment.java
@@ -22,628 +22,632 @@ import java.sql.SQLXML;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MockPreparedStatment implements PreparedStatement {
 
   private Connection connection;
   private String sql;
-  private int resultSetType;
-  private int resultSetConcurrency;
-  private int resultSetHoldability;
+//  private int resultSetType;
+//  private int resultSetConcurrency;
+//  private int resultSetHoldability;
+  private Logger logger = Logger.getLogger(this.getClass().getName());
 
   public MockPreparedStatment(final Connection connection0, final String sql0) {
     this.connection = connection0;
     this.sql = sql0;
-    resultSetType=ResultSet.TYPE_FORWARD_ONLY;
-    resultSetConcurrency=ResultSet.CONCUR_READ_ONLY;
-    resultSetHoldability=ResultSet.CLOSE_CURSORS_AT_COMMIT;
+//    resultSetType = ResultSet.TYPE_FORWARD_ONLY;
+//    resultSetConcurrency = ResultSet.CONCUR_READ_ONLY;
+//    resultSetHoldability = ResultSet.CLOSE_CURSORS_AT_COMMIT;
   }
 
   public MockPreparedStatment(Connection connection0, String sql0, int resultSetType0,
       int resultSetConcurrency0, int resultSetHoldability0) {
     this.connection = connection0;
     this.sql = sql0;
-    resultSetType=resultSetType0;
-    resultSetConcurrency=resultSetConcurrency0;
-    resultSetHoldability=resultSetHoldability0;
+//    resultSetType = resultSetType0;
+//    resultSetConcurrency = resultSetConcurrency0;
+//    resultSetHoldability = resultSetHoldability0;
   }
 
   @Override
   public ResultSet executeQuery(String sql) throws SQLException {
-    // TODO Auto-generated method stub
+    logger.log(Level.INFO, "executeQuery:" + sql);
     return null;
   }
 
   @Override
   public int executeUpdate(String sql) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
+    logger.log(Level.INFO, "executeUdpate:" + sql);
     return 0;
   }
 
   @Override
   public void close() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int getMaxFieldSize() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void setMaxFieldSize(int max) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int getMaxRows() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void setMaxRows(int max) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setEscapeProcessing(boolean enable) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int getQueryTimeout() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void setQueryTimeout(int seconds) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void cancel() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public SQLWarning getWarnings() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public void clearWarnings() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setCursorName(String name) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public boolean execute(String sql) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public ResultSet getResultSet() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public int getUpdateCount() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public boolean getMoreResults() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public void setFetchDirection(int direction) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int getFetchDirection() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void setFetchSize(int rows) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int getFetchSize() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public int getResultSetConcurrency() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public int getResultSetType() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void addBatch(String sql) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void clearBatch() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public int[] executeBatch() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public Connection getConnection() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public boolean getMoreResults(int current) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public ResultSet getGeneratedKeys() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public int executeUpdate(String sql, int autoGeneratedKeys) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public int executeUpdate(String sql, int[] columnIndexes) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public int executeUpdate(String sql, String[] columnNames) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public boolean execute(String sql, int autoGeneratedKeys) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public boolean execute(String sql, int[] columnIndexes) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public boolean execute(String sql, String[] columnNames) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public int getResultSetHoldability() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public boolean isClosed() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public void setPoolable(boolean poolable) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public boolean isPoolable() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public void closeOnCompletion() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public boolean isCloseOnCompletion() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public <T> T unwrap(Class<T> iface) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public boolean isWrapperFor(Class<?> iface) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public ResultSet executeQuery() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public int executeUpdate() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return 0;
   }
 
   @Override
   public void setNull(int parameterIndex, int sqlType) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBoolean(int parameterIndex, boolean x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setByte(int parameterIndex, byte x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setShort(int parameterIndex, short x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setInt(int parameterIndex, int x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setLong(int parameterIndex, long x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setFloat(int parameterIndex, float x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setDouble(int parameterIndex, double x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBigDecimal(int parameterIndex, BigDecimal x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setString(int parameterIndex, String x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBytes(int parameterIndex, byte[] x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setDate(int parameterIndex, Date x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setTime(int parameterIndex, Time x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setTimestamp(int parameterIndex, Timestamp x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setAsciiStream(int parameterIndex, InputStream x, int length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setUnicodeStream(int parameterIndex, InputStream x, int length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBinaryStream(int parameterIndex, InputStream x, int length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void clearParameters() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setObject(int parameterIndex, Object x, int targetSqlType) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setObject(int parameterIndex, Object x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public boolean execute() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return false;
   }
 
   @Override
   public void addBatch() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setCharacterStream(int parameterIndex, Reader reader, int length)
       throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setRef(int parameterIndex, Ref x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBlob(int parameterIndex, Blob x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setClob(int parameterIndex, Clob x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setArray(int parameterIndex, Array x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public ResultSetMetaData getMetaData() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public void setDate(int parameterIndex, Date x, Calendar cal) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setTime(int parameterIndex, Time x, Calendar cal) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setTimestamp(int parameterIndex, Timestamp x, Calendar cal) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNull(int parameterIndex, int sqlType, String typeName) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setURL(int parameterIndex, URL x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public ParameterMetaData getParameterMetaData() throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
     return null;
   }
 
   @Override
   public void setRowId(int parameterIndex, RowId x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNString(int parameterIndex, String value) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNCharacterStream(int parameterIndex, Reader value, long length)
       throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNClob(int parameterIndex, NClob value) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setClob(int parameterIndex, Reader reader, long length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBlob(int parameterIndex, InputStream inputStream, long length)
       throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNClob(int parameterIndex, Reader reader, long length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setSQLXML(int parameterIndex, SQLXML xmlObject) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setObject(int parameterIndex, Object x, int targetSqlType, int scaleOrLength)
       throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setAsciiStream(int parameterIndex, InputStream x, long length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBinaryStream(int parameterIndex, InputStream x, long length) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setCharacterStream(int parameterIndex, Reader reader, long length)
       throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setAsciiStream(int parameterIndex, InputStream x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBinaryStream(int parameterIndex, InputStream x) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setCharacterStream(int parameterIndex, Reader reader) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNCharacterStream(int parameterIndex, Reader value) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setClob(int parameterIndex, Reader reader) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setBlob(int parameterIndex, InputStream inputStream) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
   @Override
   public void setNClob(int parameterIndex, Reader reader) throws SQLException {
-    // TODO Auto-generated method stub
+    // No-op
 
   }
 
