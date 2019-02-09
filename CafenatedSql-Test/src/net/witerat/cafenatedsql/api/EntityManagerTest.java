381625ce686afaa1ae976ebc734ca8cb9c845b21
John Hutcheson <witerat.test@gmail.com>
2019-02-09 21:26:26 +1300

    Test the calculus

---
+++ CafenatedSql-Test/src/net/witerat/cafenatedsql/api/EntityManagerTest.java
@@ -0,0 +1,18 @@
+package net.witerat.cafenatedsql.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntityManagerTest {
+
+  @Test
+  public void testRefactor() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test 
+  public void testCalculus(){
+    assertEquals("ALTER",EntityManager.Calculus.ALTER.name());
+  }
+}
