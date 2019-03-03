be21c34fbaec0e7b517229e2c98217c4e4473708
John Hutcheson <witerat.test@gmail.com>
2019-03-04 12:51:06 +1300

    Rename ParameterTag to ParamTag

--- CafenatedSql-Test/test-src/net/witerat/cafenatedsql/spi/driver/DriverLoaderTest.java
+++ CafenatedSql-Test/test-src/net/witerat/cafenatedsql/spi/driver/DriverLoaderTest.java
@@ -1,30 +1,42 @@
 package net.witerat.cafenatedsql.spi.driver;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-import net.witerat.cafenatedsql.spi.driver.ResourceProtocol;
 import net.witerat.cafenatedsql.spi.driver.tags.ConnectionTag;
 import net.witerat.cafenatedsql.spi.driver.tags.DefTag;
 import net.witerat.cafenatedsql.spi.driver.tags.DialectTag;
 import net.witerat.cafenatedsql.spi.driver.tags.DriverTag;
 import net.witerat.cafenatedsql.spi.driver.tags.GrammarTag;
 import net.witerat.cafenatedsql.spi.driver.tags.NounTag;
-import net.witerat.cafenatedsql.spi.driver.tags.ParameterTag;
+import net.witerat.cafenatedsql.spi.driver.tags.ObjectFactory;
+import net.witerat.cafenatedsql.spi.driver.tags.ParamTag;
 import net.witerat.cafenatedsql.spi.driver.tags.UrlTag;
+import net.witerat.cafenatedsql.spi.driver.tags.UseDialectTag;
 import net.witerat.cafenatedsql.spi.driver.tags.UseTag;
 import net.witerat.cafenatedsql.spi.driver.tags.VerbTag;;
 
@@ -59,8 +71,56 @@ public class DriverLoaderTest {
 	public void testLoad() throws JAXBException, MalformedURLException{
 		testDriver1resource();
 		try {
-			JAXBContext jc = JAXBContext.newInstance(DriverTag.class);
+			JAXBContext jc = JAXBContext.newInstance(DriverTag.class,
+			    ObjectFactory.class);
+			
 			Unmarshaller u = jc.createUnmarshaller();
+			
+			// Create catalog resolver and set a catalog list.
+//			CatalogManager cm = new CatalogManager();
+//			cm.debug.setDebug(Integer.MAX_VALUE);
+//			CatalogResolver resolver = new CatalogResolver(cm);
+//			
+//
+//			XMLReader xr = null;
+//		        try {
+//		          xr = XMLReaderFactory.createXMLReader();
+//		        } catch (SAXException e) {
+//		          // TODO Auto-generated catch block
+//		          e.printStackTrace();
+//		          }
+//			xr.setEntityResolver(resolver);
+//			xr.setErrorHandler(new ErrorHandler() {
+//                          
+//                          @Override
+//                          public void warning(SAXParseException exception) throws SAXException {
+//                            Logger.getAnonymousLogger().log(Level.INFO, "warning", exception.toString());
+//                            
+//                          }
+//                          
+//                          @Override
+//                          public void fatalError(SAXParseException exception) throws SAXException {
+//                            Logger.getAnonymousLogger().log(Level.INFO, "fatal", exception.toString());
+//                            
+//                          }
+//                          
+//                          @Override
+//                          public void error(SAXParseException exception) throws SAXException {
+//                            Logger.getAnonymousLogger().log(Level.INFO, "error", exception.toString());
+//                            
+//                          }
+//                        });
+			Logger.getAnonymousLogger().log(Level.INFO, "CWD: "+new File(".").getAbsolutePath());
+			SchemaFactory sf=SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+                        try {
+                          final URL schemaURL = new URL("classpath:schemas/cafenated.xsd");
+                          final Schema schema = sf.newSchema(schemaURL);
+                          assertNotNull("schema not found", schema);
+                          u.setSchema(schema);
+                        } catch (SAXException e) {
+                           fail("Exception: "+e.toString());
+                        }
+			assertNotNull("No schema (cafenated.xsd) found", u.getSchema());
 			DriverTag  driver = (DriverTag) u.unmarshal(new URL("classpath:driver1.csql.xml"));
 			assertNotNull(driver);
 			assertEquals("velocity", driver.getTemplateEngine());
@@ -98,7 +158,7 @@ public class DriverLoaderTest {
 			assertEquals("Schema", g0n0.getName());
 			
 			assertEquals(1, g0n0.getParameters().size());
-			ParameterTag[] g0n0p = g0n0.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n0p = g0n0.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n0p[0]);
 			assertEquals("schema", g0n0p[0].getName());
 			assertEquals("string", g0n0p[0].getType());
@@ -108,14 +168,14 @@ public class DriverLoaderTest {
 			assertNotNull(g0n0v0);
 			assertEquals("create", g0n0v0.getName());
 			assertEquals(1, g0n0v0.getParameters().size());
-			ParameterTag[] g0n0v0p=g0n0v0.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n0v0p=g0n0v0.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n0v0p[0]);
 			assertEquals("whenMissing", g0n0v0p[0].getName());
 			VerbTag g0n0v1 = g0n0v[1];
 			assertNotNull(g0n0v1);
 			assertEquals("drop", g0n0v1.getName());
 			assertEquals(1, g0n0v1.getParameters().size());
-			ParameterTag[] g0n0v1p=g0n0v1.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n0v1p=g0n0v1.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n0v1p[0]);
 			assertEquals("whenPresent", g0n0v1p[0].getName());
 
@@ -123,7 +183,7 @@ public class DriverLoaderTest {
 			assertNotNull(g0n1);
 			assertEquals("sequence", g0n1.getName());
 			assertEquals(1, g0n1.getParameters().size());
-			ParameterTag[] g0n1p = g0n1.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n1p = g0n1.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n1p[0]);
 			assertEquals("sequenceName", g0n1p[0].getName());
 			assertEquals(1, g0n1.getVerbs().size());
@@ -137,7 +197,7 @@ public class DriverLoaderTest {
 			assertEquals("user", g0n2.getName());
 			
 			assertEquals(1, g0n2.getParameters().size());
-			ParameterTag[] g0n2p = g0n2.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n2p = g0n2.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n2p[0]);
 			assertEquals("userName", g0n2p[0].getName());
 			assertEquals("string", g0n2p[0].getType());
@@ -147,7 +207,7 @@ public class DriverLoaderTest {
 			assertNotNull(g0n2v0);
 			assertEquals("create", g0n2v0.getName());
 			assertEquals(4, g0n2v0.getParameters().size());
-			ParameterTag[] g0n2v0p=g0n2v0.getParameters().toArray(new ParameterTag[4]);
+			ParamTag[] g0n2v0p=g0n2v0.getParameters().toArray(new ParamTag[4]);
 			assertNotNull(g0n2v0p[0]);
 			assertEquals("whenMissing", g0n2v0p[0].getName());
 			assertEquals("boolean",     g0n2v0p[0].getType());
@@ -163,7 +223,7 @@ public class DriverLoaderTest {
 			VerbTag g0n2v1 = g0n2v[1];
 			assertNotNull(g0n2v1);
 			assertEquals("drop", g0n2v1.getName());
-			ParameterTag[] g0n2v1p=g0n2v1.getParameters().toArray(new ParameterTag[1]);
+			ParamTag[] g0n2v1p=g0n2v1.getParameters().toArray(new ParamTag[1]);
 			assertNotNull(g0n2v1p);
 			assertEquals("whenPresent", g0n2v1p[0].getName());
 			assertEquals("boolean",     g0n2v1p[0].getType());
@@ -173,12 +233,12 @@ public class DriverLoaderTest {
 			ConnectionTag c0=cons[0];
 			assertNotNull(c0);
 			assertEquals("memory", c0.getMethod());
-			DialectTag c0d = c0.getDialect();
-			assertNotNull(c0d);
+			UseDialectTag c0d = c0.getUseDialect();
+			assertNotNull("no use-dialect for connection: " + c0.getMethod(),c0d);
 			assertEquals("$mode", c0d.getName());
 			assertEquals("h2", c0d.getDefaultName());
 			UrlTag c0u=c0.getUrl();
-			assertEquals(3, c0u.getRawContent().size());
+			assertEquals(2, c0u.getRawContent().size());
 			Object[] c0ur=c0u.getRawContent().toArray(new Object[2]);
 			assertTrue(c0ur[0]instanceof String);
 			assertEquals("\n\t\t\tjdbc:h2:mem:\n\t\t\t", (String)c0ur[0]);
