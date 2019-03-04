4d2fd5a282fea21e37347b3910b87e8f43f6dc41
John Hutcheson <witerat.test@gmail.com>
2019-03-04 13:16:10 +1300

    Added Logging to identify JAXB fail for mixed types.
    Solved by using Eclipse Link persistence.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/VerbTag.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/VerbTag.java
@@ -1,27 +1,31 @@
 package net.witerat.cafenatedsql.spi.driver.tags;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlElementRefs;
 import javax.xml.bind.annotation.XmlMixed;
-import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * The Class <code>VerbTag</code> describes how instructions related to database
  * entities - nouns are rendered and submitted to a backend database.
  */
-@XmlType(name = "VerbType", namespace = "-//org.witerat/cafenated/sql")
+@XmlType
 public class VerbTag {
   /** The rawContent property. */
   private Collection<Object> rawContent;
   /** The parametersByName property. */
-  private Map<String, ParameterTag> parametersByName;
+  private Map<String, ParamTag> parametersByName;
   /** The name. */
   private String name;
 
@@ -35,13 +39,6 @@ public class VerbTag {
     return name;
   }
 
-  /**
-   * Get additional tags and text.
-   * @return strings and tags
-   */
-  public Collection<Object> getRawContent() {
-    return rawContent;
-  }
 
   /**
    * Sets the name.
@@ -58,29 +55,46 @@ public class VerbTag {
    * @param rawContent0
    *          strings and tags
    */
-  @XmlElementRefs({ @XmlElementRef(name = "param", type = ParameterTag.class),
-      @XmlElementRef(name = "use", type = UseTag.class) })
   @XmlMixed
+  @XmlElementRefs(
+      {@XmlElementRef(namespace = "-//org.witerat/cafenated/sql",
+          type = ParamTag.class, name = "param"),
+       @XmlElementRef(namespace = "-//org.witerat/cafenated/sql",
+           type = UseTag.class, name = "use") })
   public void setRawContent(final Collection<Object> rawContent0) {
     Collection<Object> rc = new ArrayList<>();
+    Logger.getAnonymousLogger().log(Level.INFO,
+        ToStringBuilder.reflectionToString(rawContent0));
     for (Object o : rawContent0) {
-      if (o instanceof ParameterTag) {
-        setParameter((ParameterTag) o);
+      if (o instanceof ParamTag) {
+        setParameter((ParamTag) o);
       } else if (!rc.isEmpty() || (!(o instanceof String))
           || !(((String) o).trim().isEmpty())) {
         rc.add(o);
       }
     }
+    Logger.getAnonymousLogger().log(Level.INFO,
+        MessageFormat.format("verb {0}: {1}\n",
+         this.name, ToStringBuilder.reflectionToString(rc)));
     this.rawContent = rc;
   }
-
+  /**
+   * The raw content.
+   * @return a collection of raw content.
+   */
+  public Collection<Object> getRawContent() {
+//    if (rawContent == null) {
+//      return new ArrayList<Object>();
+//    }
+    return rawContent;
+  }
   /**
    * Sets a parameter.
    *
    * @param param
    *          the new parameter
    */
-  private void setParameter(final ParameterTag param) {
+  public void setParameter(final ParamTag param) {
     if (parametersByName == null) {
       parametersByName = new LinkedHashMap<>();
     }
@@ -91,8 +105,7 @@ public class VerbTag {
    * get parameter tags.
    * @return the parameters
    */
-  @XmlTransient
-  public Collection<ParameterTag> getParameters() {
+  public Collection<ParamTag> getParameters() {
     if (parametersByName == null) {
       parametersByName = new LinkedHashMap<>();
     }
