3fea4fdca3bc122584cb63d3dd95d3784e3a7f19
John Hutcheson <witerat.test@gmail.com>
2021-07-15 02:28:22 +1200

    Javadoc

--- CafenatedSql/src/net/witerat/cafenatedsql/api/GrammarFactory.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/api/GrammarFactory.java
@@ -4,7 +4,7 @@ import net.witerat.cafenatedsql.api.driver.DialectDef;
 import net.witerat.cafenatedsql.api.driver.GrammarDef;
 
 /**
- * A factory contract for creating Grammar objects.
+ * A factory contract for creating Grammar objects from dialect definitions.
  */
 public interface GrammarFactory extends CafenatedFactory {
 
@@ -12,9 +12,9 @@ public interface GrammarFactory extends CafenatedFactory {
    * New grammar.
    *
    * @param dd
-   *          the dd
+   *          the dialect definition.
    * @param name
-   *          the name
+   *          the name for the new grammar, null for anonymous grammars.
    * @return A new grammar definition
    */
   GrammarDef newGrammar(DialectDef dd, String name);
