14eb3fc903ea1b1e3293f2aef149a533ab52cb17
John Hutcheson <witerat.test@gmail.com>
2021-07-18 12:11:39 +1200

    Javadoc and formatting.

--- CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/AbstractMacroLocator.java
+++ CafenatedSql/src/net/witerat/cafenatedsql/spi/driver/tags/AbstractMacroLocator.java
@@ -1,23 +1,34 @@
 package net.witerat.cafenatedsql.spi.driver.tags;
 
+import java.util.Iterator;
+
 import javax.xml.bind.annotation.XmlTransient;
 
 import net.witerat.cafenatedsql.spi.driver.MacroLocator;
 
 /**
  * @author John Hutcheson &lt;witerat.test@gmail.com&gt;
- *
+ * TODO Check if this class is needed, it defines no concrete methods, or
+ *      fields.
  */
 @XmlTransient
-public abstract class AbstractMacroLocator implements MacroLocator {
+public abstract class AbstractMacroLocator implements MacroLocator,
+  Iterable<MacroTag> {
 
-  /*
-   * (non-Javadoc)
+  /**
+   * {@inheritDoc}
    *
-   * @see
-   * net.witerat.cafenatedsql.spi.driver.MacroLocator#getMacro(java.lang.String)
+   * @see net.witerat.cafenatedsql.spi.driver.MacroLocator#
+   *    getMacro(java.lang.String)
    */
   @Override
   public abstract MacroTag getMacro(String name);
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see java.lang.Iterable#iterator()
+   */
+  public abstract Iterator<MacroTag> iterator();
 
 }
